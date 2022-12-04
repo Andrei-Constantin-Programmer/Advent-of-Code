5bfdaccb98f329f547f2f44fe2c48dcfff64376d
Andrei Constantin <andrei.constantin@outlook.com>
2022-12-04 19:39:03 +0000

    Solved Year 2019 Day 1 Part 2

--- Advent of Code/Challenge Solutions/Year 2019/ChallengeSolution1.cs
+++ Advent of Code/Challenge Solutions/Year 2019/ChallengeSolution1.cs
@@ -7,24 +7,39 @@ namespace Advent_of_Code.Challenge_Solutions.Year_2019
         public void SolveFirstPart()
         {
             Console.WriteLine(ReadModuleMasses()
-                .Select(mass => CalculateMassSum(mass))
+                .Select(mass => CalculateFuelForMass(mass))
                 .Sum());
         }
 
         public void SolveSecondPart()
         {
-            throw new NotImplementedException();
+            Console.WriteLine(ReadModuleMasses()
+                .Select(mass =>
+                {
+                    var remainingMass = mass;
+                    long totalFuel = 0;
+
+                    long fuel;
+                    while((fuel = CalculateFuelForMass(remainingMass)) > 0)
+                    {
+                        totalFuel += fuel;
+                        remainingMass = fuel;
+                    }
+
+                    return totalFuel;
+                })
+                .Sum());
         }
 
-        private long CalculateMassSum(long moduleMass)
+        private long CalculateFuelForMass(long moduleMass)
         {
             return moduleMass / 3 - 2;
         }
 
-        private List<int> ReadModuleMasses()
+        private List<long> ReadModuleMasses()
         {
             return File.ReadAllLines(GetFileString(FileType.Input, 2019, 1))
-                .Select(massString => Convert.ToInt32(massString))
+                .Select(massString => Convert.ToInt64(massString))
                 .ToList();
         }
     }
