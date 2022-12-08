bdc2da3986942777c4dba79af5d9173637c42bd4
Andrei Constantin <andrei.constantin@outlook.com>
2022-12-08 22:50:59 +0000

    (WIP) Refactoring & modifications to the second part of the challenge (Year 2021, Day 9)

--- Advent of Code/Challenge Solutions/Year 2021/ChallengeSolution9.cs
+++ Advent of Code/Challenge Solutions/Year 2021/ChallengeSolution9.cs
@@ -4,118 +4,93 @@ namespace Advent_of_Code.Challenge_Solutions.Year_2021
 {
     class ChallengeSolution9 : ChallengeSolution
     {
-        private int n, m;
+        private int rows, columns;
 
         public ChallengeSolution9()
         {
             string[] lines = File.ReadAllLines(GetFileString(FileType.Input, 2021, 9));
-            n = lines.Length;
-            m = lines[0].Length;
+            rows = lines.Length;
+            columns = lines[0].Length;
         }
 
         public void SolveFirstPart()
         {
             int[,] heightMap = ReadHeightMap();
-            using (TextReader read = GetInputFile(2021, 9))
+            int sum = 0;
+            var lowPoints = GetLowPoints(heightMap);
+
+            foreach (var point in lowPoints)
             {
-                for (int i = 0; i < n; i++)
-                {
-                    char[] heightsAsChar = read.ReadLine().ToCharArray();
-                    byte[] heights = Array.ConvertAll(heightsAsChar, character => (byte)Char.GetNumericValue(character));
-                    for (int j = 0; j < m; j++)
-                        heightMap[i, j] = heights[j];
-                }
-
-                int sum = 0;
-                var lowPoints = GetLowPoints(heightMap);
-
-                foreach (var point in lowPoints)
-                {
-                    sum += heightMap[point.Key, point.Value] + 1;
-                }
-
-                Console.WriteLine(sum);
+                sum += heightMap[point.row, point.column] + 1;
             }
+
+            Console.WriteLine(sum);
         }
 
         public void SolveSecondPart()
         {
-            throw new NotImplementedException();
-
-            int[,] heightMap = ReadHeightMap();
-
+            var heightMap = ReadHeightMap();
             var lowPoints = GetLowPoints(heightMap);
-            var basinSizes = new List<int>();
 
-            int[,] basinMap = (int[,])heightMap.Clone();
+            var basinHeights = CalculateBasinSizes(heightMap, lowPoints);
 
-            foreach (var point in lowPoints)
+            Console.WriteLine(basinHeights
+                .OrderBy(x => x)
+                .Take(3)
+                .Aggregate((x, y) => x * y));
+        }
+
+        private List<int> CalculateBasinSizes(int[,] heightMap, List<(int row, int column)> lowPoints)
+        {
+            var basinHeights = new List<int>();
+
+            foreach (var lowPoint in lowPoints)
             {
-                int size = 1;
-                basinMap[point.Key, point.Value] = 9;
-                var neighbours = GetNeighbourLowPoints(basinMap, point);
-                
-                while(neighbours.Count>0)
+                if (heightMap[lowPoint.row, lowPoint.column] < 9)
                 {
-                    var newNeighbours = new List<KeyValuePair<int, int>>();
-
-                    size += neighbours.Count;
-                    foreach(var neighbour in neighbours)
+                    int basinSize;
+                    CreateBasin(heightMap, lowPoint, out basinSize);
+                    for (var i = 0; i < rows; i++)
                     {
-                        basinMap[neighbour.Key, neighbour.Value] = 9;
-                        newNeighbours.AddRange(GetNeighbourLowPoints(basinMap, neighbour));
+                        for (var j = 0; j < columns; j++)
+                        {
+                            Console.Write(heightMap[i, j] + " ");
+                        }
+                        Console.WriteLine();
                     }
+                    Console.WriteLine();
 
-                    neighbours = newNeighbours;
+                    basinHeights.Add(basinSize);
                 }
-
-                basinSizes.Add(size);
             }
 
-            basinSizes = basinSizes.OrderByDescending(x => x).ToList();
-
-            foreach(var x in basinSizes)
-                Console.WriteLine(x);
-
-            Console.WriteLine(basinSizes[0]*basinSizes[1]*basinSizes[2]);
+            return basinHeights;
         }
 
-        private int[,] ReadHeightMap()
+        private void CreateBasin(int[,] heightMap, (int row, int column) point, out int basinSize)
         {
-            var heightMap = new int[n, m];
-            using (TextReader read = GetInputFile(2021, 9))
+            basinSize = 1;
+            heightMap[point.row, point.column] = 9;
+            var neighbours = GetNeighbourLowPoints(heightMap, point);
+
+            while(neighbours.Count > 0)
             {
-                for (int i = 0; i < n; i++)
+                var newNeighbours = new List<(int row, int column)>();
+
+                basinSize += neighbours.Count;
+                foreach(var neighbour in neighbours)
                 {
-                    char[] heightsAsChar = read.ReadLine().ToCharArray();
-                    byte[] heights = Array.ConvertAll(heightsAsChar, character => (byte)Char.GetNumericValue(character));
-                    for (int j = 0; j < m; j++)
-                        heightMap[i, j] = heights[j];
+                    heightMap[neighbour.row, neighbour.column] = 9;
+                    newNeighbours.AddRange(GetNeighbourLowPoints(heightMap, neighbour));
                 }
 
-                return heightMap;
+                neighbours = newNeighbours;
             }
         }
 
-        private List<KeyValuePair<int, int>> GetLowPoints(int[,] heightMap)
+        private List<(int row, int column)> GetNeighbourLowPoints(int[,] heightMap, (int row, int column) coord)
         {
-            var lowPoints = new List<KeyValuePair<int, int>>();
-
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < m; j++)
-                {
-                    if (IsLowPoint(heightMap, new KeyValuePair<int, int>(i, j)))
-                    {
-                        lowPoints.Add(new KeyValuePair<int, int>(i, j));
-                    }
-                }
-
-            return lowPoints;
-        }
-
-        private List<KeyValuePair<int, int>> GetNeighbourLowPoints(int[,] heightMap, KeyValuePair<int, int> coord)
-        {
-            var neighbours = new List<KeyValuePair<int, int>>();
+            var neighbours = new List<(int row, int column)>();
             var coords = GetNeighbourCoords(coord);
             foreach (var x in coords)
             {
@@ -126,27 +101,46 @@ namespace Advent_of_Code.Challenge_Solutions.Year_2021
             return neighbours;
         }
 
-        private bool IsLowPoint(int[,] heightMap, KeyValuePair<int, int> coord)
+        private List<(int row, int column)> GetLowPoints(int[,] heightMap)
         {
-            int i = coord.Key, j = coord.Value;
-            int x = heightMap[i, j];
-            bool isLowPoint = true;
-            if (i - 1 >= 0 && heightMap[i - 1, j] <= x)
-                isLowPoint = false;
-            if (j - 1 >= 0 && heightMap[i, j - 1] <= x)
-                isLowPoint = false;
-            if (i + 1 < n && heightMap[i + 1, j] <= x)
-                isLowPoint = false;
-            if (j + 1 < m && heightMap[i, j + 1] <= x)
-                isLowPoint = false;
+            var lowPoints = new List<(int row, int column)>();
 
-            return isLowPoint;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (IsLowPoint(heightMap, (i, j)))
+                    {
+                        lowPoints.Add((i, j));
+                    }
+                }
+            }
+
+            return lowPoints;
         }
 
-        private List<KeyValuePair<int, int>> GetNeighbourCoords(KeyValuePair<int, int> coord)
+        private bool IsLowPoint(int[,] heightMap, (int row, int column) coord)
         {
-            int i = coord.Key, j = coord.Value;
-            var coords = new List<KeyValuePair<int, int>>() { new KeyValuePair<int, int>(i - 1, j), new KeyValuePair<int, int>(i, j - 1), new KeyValuePair<int, int>(i + 1, j), new KeyValuePair<int, int>(i, j + 1) };
+            foreach(var neighbour in GetNeighbourCoords(coord))
+            {
+                if (InBounds(coord) && heightMap[neighbour.row, neighbour.column] <= heightMap[coord.row, coord.column])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private List<(int row, int column)> GetNeighbourCoords((int row, int column) coord)
+        {
+            var coords = new List<(int row, int column)>() 
+            { 
+                (coord.row - 1, coord.column), 
+                (coord.row, coord.column - 1), 
+                (coord.row + 1, coord.column), 
+                (coord.row, coord.column + 1) 
+            };
 
             for (int k = 0; k < coords.Count; k++)
                 if (!InBounds(coords[k]))
@@ -158,9 +152,27 @@ namespace Advent_of_Code.Challenge_Solutions.Year_2021
             return coords;
         }
 
-        private bool InBounds(KeyValuePair<int, int> coord)
+        private bool InBounds((int row, int column) coord)
         {
-            return coord.Key >= 0 && coord.Key < n && coord.Value >= 0 && coord.Value < m;
+            return coord.row >= 0 && coord.row < rows && coord.column >= 0 && coord.column < columns;
         }
+
+        private int[,] ReadHeightMap()
+        {
+            var heightMap = new int[rows, columns];
+            using (TextReader read = GetInputFile(2021, 9))
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    char[] heightsAsChar = read.ReadLine().ToCharArray();
+                    byte[] heights = Array.ConvertAll(heightsAsChar, character => (byte)Char.GetNumericValue(character));
+                    for (int j = 0; j < columns; j++)
+                        heightMap[i, j] = heights[j];
+                }
+
+                return heightMap;
+            }
+        }
+
     }
 }
