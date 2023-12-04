efb4101574ff597a91d626b03c2ca3b0c571228f
Andrei Constantin <andrei.constantin@outlook.com>
2023-12-04 01:30:54 +0000

    Solved Y2023 D1 P2

--- Advent of Code/Challenge Solutions/Year 2023/ChallengeSolution01.cs
+++ Advent of Code/Challenge Solutions/Year 2023/ChallengeSolution01.cs
@@ -4,7 +4,33 @@ namespace Advent_of_Code.Challenge_Solutions.Year_2023;
 
 internal class ChallengeSolution01 : ChallengeSolution
 {
+    private readonly Dictionary<string, char> _digitWords = new()
+    {
+        { "zero", '0' },
+        { "one", '1' },
+        { "two", '2' },
+        { "three", '3' },
+        { "four", '4' },
+        { "five", '5' },
+        { "six", '6' },
+        { "seven", '7' },
+        { "eight", '8' },
+        { "nine", '9' }
+    };
+
     protected override void SolveFirstPart()
+    {
+        int calibrationSum = ComputeCalibrationSum(false);
+        Console.WriteLine(calibrationSum);
+    }
+
+    protected override void SolveSecondPart()
+    {
+        int calibrationSum = ComputeCalibrationSum(true);
+        Console.WriteLine(calibrationSum);
+    }
+
+    private int ComputeCalibrationSum(bool includeWords)
     {
         int calibrationSum = 0;
 
@@ -12,24 +38,59 @@ internal class ChallengeSolution01 : ChallengeSolution
         using TextReader read = Reader.GetInputFile(2023, 1);
         while ((line = read.ReadLine()) != null)
         {
-            calibrationSum += ComputeCalibrationValue(line);
+            calibrationSum += ComputeCalibrationValue(line, includeWords);
         }
 
-        Console.WriteLine(calibrationSum);
+        return calibrationSum;
     }
 
-    protected override void SolveSecondPart()
+    private int ComputeCalibrationValue(string line, bool doIncludeWords)
     {
-        throw new NotImplementedException();
-    }
+        char firstDigit = line.FirstOrDefault(char.IsDigit);
+        char lastDigit = line.LastOrDefault(char.IsDigit);
 
-    private static int ComputeCalibrationValue(string line)
-    {
-        char firstDigit = line.First(c => char.IsDigit(c));
-        char lastDigit = line.Last(c => char.IsDigit(c));
+        if (doIncludeWords)
+        {
+            firstDigit = FindFirstDigitWord(line, firstDigit);
+            lastDigit = FindLastDigitWord(line, lastDigit);
+        }
 
         string calibrationValue = $"{firstDigit}{lastDigit}";
 
-        return int.Parse(calibrationValue);
+        return int.TryParse(calibrationValue, out int calibrationNumber) ? calibrationNumber : 0;
+    }
+
+    private char FindFirstDigitWord(string line, char currentFirstDigit)
+    {
+        var firstDigitIndex = line.IndexOf(currentFirstDigit);
+
+        foreach (var word in _digitWords.Keys)
+        {
+            var firstWordIndex = line.IndexOf(word);
+            if (firstDigitIndex == -1 || (firstWordIndex > -1 && firstDigitIndex > firstWordIndex))
+            {
+                firstDigitIndex = firstWordIndex;
+                currentFirstDigit = _digitWords[word];
+            }
+        }
+
+        return currentFirstDigit;
+    }
+
+    private char FindLastDigitWord(string line, char currentLastDigit)
+    {
+        var lastDigitIndex = line.LastIndexOf(currentLastDigit);
+
+        foreach (var word in _digitWords.Keys)
+        {
+            var lastWordIndex = line.LastIndexOf(word);
+            if (lastDigitIndex == -1 || (lastWordIndex > -1 && lastDigitIndex < lastWordIndex))
+            {
+                lastDigitIndex = lastWordIndex;
+                currentLastDigit = _digitWords[word];
+            }
+        }
+
+        return currentLastDigit;
     }
 }
