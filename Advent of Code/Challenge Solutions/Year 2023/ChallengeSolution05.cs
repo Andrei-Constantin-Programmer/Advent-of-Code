3662b1ed011f0a0faa70765db90e3d11d07aebcd
Andrei Constantin <andrei.constantin@outlook.com>
2023-12-06 17:34:09 +0000

    Solved Y2023 D5 P1

--- Advent of Code/Challenge Solutions/Year 2023/ChallengeSolution05.cs
+++ Advent of Code/Challenge Solutions/Year 2023/ChallengeSolution05.cs
@@ -2,7 +2,8 @@
 
 internal class ChallengeSolution05 : ChallengeSolution
 {
-    private readonly List<int> _seeds;
+    private readonly List<string> _lines;
+
     private readonly List<Mapping> _seedToSoilMappings;
     private readonly List<Mapping> _soilToFertilizerMappings;
     private readonly List<Mapping> _fertilizerToWaterMappings;
@@ -13,27 +14,39 @@ internal class ChallengeSolution05 : ChallengeSolution
 
     public ChallengeSolution05()
     {
-        var lines = ReadInputLines();
+        _lines = ReadInputLines();
 
-        _seeds = ParseSeeds(lines[0]);
-        _seedToSoilMappings = ParseMappings(lines, "seed-to-soil map:", "soil-to-fertilizer map:");
-        _soilToFertilizerMappings = ParseMappings(lines, "soil-to-fertilizer map:", "fertilizer-to-water map:");
-        _fertilizerToWaterMappings = ParseMappings(lines, "fertilizer-to-water map:", "water-to-light map:");
-        _waterToLightMappings = ParseMappings(lines, "water-to-light map:", "light-to-temperature map:");
-        _lightToTemperatureMappings = ParseMappings(lines, "light-to-temperature map:", "temperature-to-humidity map:");
-        _temperatureToHumidityMappings = ParseMappings(lines, "temperature-to-humidity map:", "humidity-to-location map:");
-        _humidityToLocationMappings = ParseMappings(lines, "humidity-to-location map:", lines.Last());
+        _seedToSoilMappings = ParseMappings(_lines, "seed-to-soil map:", "soil-to-fertilizer map:");
+        _soilToFertilizerMappings = ParseMappings(_lines, "soil-to-fertilizer map:", "fertilizer-to-water map:");
+        _fertilizerToWaterMappings = ParseMappings(_lines, "fertilizer-to-water map:", "water-to-light map:");
+        _waterToLightMappings = ParseMappings(_lines, "water-to-light map:", "light-to-temperature map:");
+        _lightToTemperatureMappings = ParseMappings(_lines, "light-to-temperature map:", "temperature-to-humidity map:");
+        _temperatureToHumidityMappings = ParseMappings(_lines, "temperature-to-humidity map:", "humidity-to-location map:");
+        _humidityToLocationMappings = ParseMappings(_lines, "humidity-to-location map:", _lines.Last());
     }
 
     protected override void SolveFirstPart()
     {
-        throw new NotImplementedException();
+        var minimumLocation = ParseSeeds(_lines[0])
+            .Select(MapSeedToLocation)
+            .Min();
+
+        Console.WriteLine(minimumLocation);
     }
 
     protected override void SolveSecondPart()
     {
         throw new NotImplementedException();
     }
+
+    private long MapSeedToLocation(long seed) =>
+        _humidityToLocationMappings.Map(
+            _temperatureToHumidityMappings.Map(
+                _lightToTemperatureMappings.Map(
+                    _waterToLightMappings.Map(
+                        _fertilizerToWaterMappings.Map(
+                            _soilToFertilizerMappings.Map(
+                                _seedToSoilMappings.Map(seed)))))));
 
     private static List<Mapping> ParseMappings(List<string> inputLines, string startMarker, string endMarker)
     {
@@ -43,23 +56,22 @@ internal class ChallengeSolution05 : ChallengeSolution
         {
             var mappingElements = inputLines[i]
                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
+                .Select(long.Parse)
                 .ToList();
 
             mappings.Add(new(
-                Range: new Range(
-                    start: Math.Min(mappingElements[0], mappingElements[1]),
-                    end: Math.Max(mappingElements[0], mappingElements[1])),
+                DestinationRangeStart: mappingElements[0],
+                SourceRangeStart: mappingElements[1],
                 RangeLength: mappingElements[2]));
         }
 
         return mappings;
     }
 
-    private static List<int> ParseSeeds(string firstLine) => firstLine
+    private static List<long> ParseSeeds(string firstLine) => firstLine
         .Split(": ", StringSplitOptions.RemoveEmptyEntries)[1]
         .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-        .Select(int.Parse)
+        .Select(long.Parse)
         .ToList();
 
     private static List<string> ReadInputLines()
@@ -74,5 +86,18 @@ internal class ChallengeSolution05 : ChallengeSolution
         return lines;
     }
 
-    private record Mapping(Range Range, long RangeLength);
 }
+
+public static class MappingExtensions
+{
+    public static long Map(this List<Mapping> mappings, long value)
+    {
+        Mapping? mapping = mappings.FirstOrDefault(m => m.SourceRangeStart <= value && value <= m.SourceRangeStart + m.RangeLength);
+
+        return mapping is null
+            ? value
+            : mapping.DestinationRangeStart + (value - mapping.SourceRangeStart);
+    }
+}
+
+public record Mapping(long DestinationRangeStart, long SourceRangeStart, long RangeLength);
