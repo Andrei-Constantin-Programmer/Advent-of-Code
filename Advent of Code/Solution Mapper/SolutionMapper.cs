a63be50f99cda66fc9afebd5587079e7db6f4706
Andrei Constantin <andrei.constantin@outlook.com>
2023-12-06 15:03:22 +0000

    Refactored the SolutionMapper to use reflection instead of hard-coded values

--- Advent of Code/Solution Mapper/SolutionMapper.cs
+++ Advent of Code/Solution Mapper/SolutionMapper.cs
@@ -1,117 +1,42 @@
 ﻿using Advent_of_Code.Challenge_Solutions;
-using Year2019 = Advent_of_Code.Challenge_Solutions.Year_2019;
-using Year2020 = Advent_of_Code.Challenge_Solutions.Year_2020;
-using Year2021 = Advent_of_Code.Challenge_Solutions.Year_2021;
-using Year2022 = Advent_of_Code.Challenge_Solutions.Year_2022;
-using Year2023 = Advent_of_Code.Challenge_Solutions.Year_2023;
+using System.Reflection;
 
 namespace Advent_of_Code.Solution_Mapper;
 
 internal class SolutionMapper : ISolutionMapper
 {
-    private readonly Dictionary<int, Func<int, ChallengeSolution>> _yearMappings;
+    private const string YEAR_NAMESPACE_PREFIX = "Advent_of_Code.Challenge_Solutions.Year_";
+    private readonly List<int> _years;
 
     public SolutionMapper()
     {
-        _yearMappings = InitialiseYearMappings();
+        var currentAssembly = Assembly.GetExecutingAssembly();
+
+        _years = currentAssembly.GetTypes()
+            .Where(type => type.Namespace is not null && type.Namespace.StartsWith(YEAR_NAMESPACE_PREFIX))
+            .Select(type => int.TryParse(type.Namespace!.Split('_').Last(), out int year) ? year : -1)
+            .Where(year => year != -1)
+            .Distinct()
+            .ToList();
     }
 
-    public bool DoesYearExist(int year)
-    {
-        return _yearMappings.ContainsKey(year);
-    }
+    public bool DoesYearExist(int year) => _years.Contains(year);
 
     public ChallengeSolution GetChallengeSolution(int year, int day)
     {
-        return _yearMappings[year](day);
-    }
+        string challengeClassName = $"ChallengeSolution{(day < 10 ? $"0{day}" : $"{day}")}";
+        string fullClassName = $"{GetChallengeYearNamespace(year)}.{challengeClassName}";
 
-    private static Dictionary<int, Func<int, ChallengeSolution>> InitialiseYearMappings()
-    {
-        return new Dictionary<int, Func<int, ChallengeSolution>>()
+        var type = Type.GetType(fullClassName);
+        if (type is null)
         {
-            [2019] = (day) => day switch
-            {
-                1 => new Year2019.ChallengeSolution01(),
-                2 => new Year2019.ChallengeSolution02(),
+            throw new NonexistentChallengeException(year, day);
+        }
 
-                _ => throw new NonexistentChallengeException(2019, day)
-            },
-            [2020] = (day) => day switch
-            {
-                1 => new Year2020.ChallengeSolution01(),
-                2 => new Year2020.ChallengeSolution02(),
-                3 => new Year2020.ChallengeSolution03(),
-                4 => new Year2020.ChallengeSolution04(),
-                5 => new Year2020.ChallengeSolution05(),
-                6 => new Year2020.ChallengeSolution06(),
-                7 => new Year2020.ChallengeSolution07(),
-                8 => new Year2020.ChallengeSolution08(),
-                9 => new Year2020.ChallengeSolution09(),
-                10 => new Year2020.ChallengeSolution10(),
-                11 => new Year2020.ChallengeSolution11(),
-                12 => new Year2020.ChallengeSolution12(),
-                13 => new Year2020.ChallengeSolution13(),
-                14 => new Year2020.ChallengeSolution14(),
-                15 => new Year2020.ChallengeSolution15(),
-                16 => new Year2020.ChallengeSolution16(),
-
-                _ => throw new NonexistentChallengeException(2020, day)
-            },
-            [2021] = (day) => day switch
-            {
-                1 => new Year2021.ChallengeSolution01(),
-                2 => new Year2021.ChallengeSolution02(),
-                3 => new Year2021.ChallengeSolution03(),
-                4 => new Year2021.ChallengeSolution04(),
-                5 => new Year2021.ChallengeSolution05(),
-                6 => new Year2021.ChallengeSolution06(),
-                7 => new Year2021.ChallengeSolution07(),
-                8 => new Year2021.ChallengeSolution08(),
-                9 => new Year2021.ChallengeSolution09(),
-                10 => new Year2021.ChallengeSolution10(),
-                11 => new Year2021.ChallengeSolution11(),
-                12 => new Year2021.ChallengeSolution12(),
-                13 => new Year2021.ChallengeSolution13(),
-                14 => new Year2021.ChallengeSolution14(),
-                15 => new Year2021.ChallengeSolution15(),
-                16 => new Year2021.ChallengeSolution16(),
-
-                _ => throw new NonexistentChallengeException(2021, day)
-            },
-            [2022] = (day) => day switch
-            {
-                1 => new Year2022.ChallengeSolution01(),
-                2 => new Year2022.ChallengeSolution02(),
-                3 => new Year2022.ChallengeSolution03(),
-                4 => new Year2022.ChallengeSolution04(),
-                5 => new Year2022.ChallengeSolution05(),
-                6 => new Year2022.ChallengeSolution06(),
-                7 => new Year2022.ChallengeSolution07(),
-                8 => new Year2022.ChallengeSolution08(),
-                9 => new Year2022.ChallengeSolution09(),
-                10 => new Year2022.ChallengeSolution10(),
-                11 => new Year2022.ChallengeSolution11(),
-                12 => new Year2022.ChallengeSolution12(),
-                13 => new Year2022.ChallengeSolution13(),
-                14 => new Year2022.ChallengeSolution14(),
-                15 => new Year2022.ChallengeSolution15(),
-                16 => new Year2022.ChallengeSolution16(),
-                17 => new Year2022.ChallengeSolution17(),
-                20 => new Year2022.ChallengeSolution20(),
-                21 => new Year2022.ChallengeSolution21(),
-                25 => new Year2022.ChallengeSolution25(),
-
-                _ => throw new NonexistentChallengeException(2022, day)
-            },
-            [2023] = (day) => day switch
-            {
-                1 => new Year2023.ChallengeSolution01(),
-                2 => new Year2023.ChallengeSolution02(),
-                3 => new Year2023.ChallengeSolution03(),
-
-                _ => throw new NonexistentChallengeException(2023, day)
-            }
-        };
+        return Activator.CreateInstance(type) is ChallengeSolution solution
+            ? solution
+            : throw new Exception($"The solution for challenge {year}_{day} is malformed");
     }
+
+    private static string GetChallengeYearNamespace(int year) => $"{YEAR_NAMESPACE_PREFIX}{year}";
 }
