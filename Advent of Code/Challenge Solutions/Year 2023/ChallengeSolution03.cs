ee787f85575bd0dbb5fc802744aa3e91f5b73258
Andrei Constantin <andrei.constantin@outlook.com>
2023-12-06 02:12:20 +0000

    Solved Y2023 D3 P2

--- Advent of Code/Challenge Solutions/Year 2023/ChallengeSolution03.cs
+++ Advent of Code/Challenge Solutions/Year 2023/ChallengeSolution03.cs
@@ -37,7 +37,33 @@ internal class ChallengeSolution03 : ChallengeSolution
 
     protected override void SolveSecondPart()
     {
-        throw new NotImplementedException();
+        Dictionary<(int row, int column), List<int>> gears = new();
+
+        for (int row = 0; row < _engineSchematic.GetLength(0); row++)
+        {
+            List<(int index, char character)> digitSequence = new();
+
+            for (int column = 0; column < _engineSchematic.GetLength(1); column++)
+            {
+                if (char.IsDigit(_engineSchematic[row, column]))
+                {
+                    digitSequence.Add((column, _engineSchematic[row, column]));
+                    continue;
+                }
+
+                ProcessGearNumberAddition(gears, row, digitSequence);
+                digitSequence.Clear();
+            }
+
+            ProcessGearNumberAddition(gears, row, digitSequence);
+        }
+
+        int totalGearRatioSum = gears.Keys
+            .Where(gearPos => gears[gearPos].Count == 2)
+            .Select(gearPos => gears[gearPos].Aggregate((acc, x) => acc * x))
+            .Sum();
+
+        Console.WriteLine(totalGearRatioSum);
     }
 
     private int ProcessDigitSequence(int row, List<(int index, char character)> digitSequence)
@@ -45,21 +71,22 @@ internal class ChallengeSolution03 : ChallengeSolution
             ? GetPartNumber(row, digitSequence)
             : 0;
 
-    private int GetPartNumber(int row, List<(int index, char character)> currentNumber) =>
-        currentNumber.Any(digit => IsAdjacentToSymbol(row, digit.index))
-            ? int.Parse(string.Concat(currentNumber.Select(digit => digit.character)))
+    private int GetPartNumber(int row, List<(int index, char character)> digitSequence) =>
+        digitSequence.Any(digit => IsAdjacentToSymbol(row, digit.index))
+            ? int.Parse(string.Concat(digitSequence.Select(digit => digit.character)))
             : 0;
-
-    private bool IsAdjacentToSymbol(int i, int j)
+    
+    private bool IsAdjacentToSymbol(int row, int column)
     {
         int rows = _engineSchematic.GetLength(0);
         int columns = _engineSchematic.GetLength(1);
 
-        for (int x = Math.Max(0, i - 1); x <= Math.Min(rows - 1, i + 1); x++)
+        for (int neighbourRow = Math.Max(0, row - 1); neighbourRow <= Math.Min(rows - 1, row + 1); neighbourRow++)
         {
-            for (int y = Math.Max(0, j - 1); y <= Math.Min(columns - 1, j + 1); y++)
+            for (int neighbourColumn = Math.Max(0, column - 1); neighbourColumn <= Math.Min(columns - 1, column + 1); neighbourColumn++)
             {
-                if ((x != i || y != j) && IsSymbol(_engineSchematic[x, y]))
+                if ((neighbourRow != row || neighbourColumn != column) 
+                    && IsSymbol(_engineSchematic[neighbourRow, neighbourColumn]))
                 {
                     return true;
                 }
@@ -69,6 +96,47 @@ internal class ChallengeSolution03 : ChallengeSolution
         return false;
 
         static bool IsSymbol(char character) => !char.IsDigit(character) && character != '.';
+    }
+
+    private void ProcessGearNumberAddition(Dictionary<(int row, int column), List<int>> gears, int row, List<(int index, char character)> digitSequence)
+    {
+        (int row, int column) gearPosition = (0, 0);
+        if (!digitSequence.Any(digit => TryGetAdjacentGearPosition(row, digit.index, out gearPosition)))
+        {
+            return;
+        }
+
+        if (gears.ContainsKey(gearPosition))
+        {
+            gears[gearPosition].Add(GetPartNumber(row, digitSequence));
+        }
+        else
+        {
+            gears[gearPosition] = new List<int>() { GetPartNumber(row, digitSequence) };
+        }
+    }
+
+    // We assume no numbers are adjacent to more than one gear
+    private bool TryGetAdjacentGearPosition(int row, int column, out (int row, int column) gearPosition)
+    {
+        int rows = _engineSchematic.GetLength(0);
+        int columns = _engineSchematic.GetLength(1);
+
+        for (int neighbourRow = Math.Max(0, row - 1); neighbourRow <= Math.Min(rows - 1, row + 1); neighbourRow++)
+        {
+            for (int neighbourColumn = Math.Max(0, column - 1); neighbourColumn <= Math.Min(columns - 1, column + 1); neighbourColumn++)
+            {
+                if ((neighbourRow != row || neighbourColumn != column)
+                    && _engineSchematic[neighbourRow, neighbourColumn] == '*')
+                {
+                    gearPosition = (neighbourRow, neighbourColumn);
+                    return true;
+                }
+            }
+        }
+
+        gearPosition = (-1, -1);
+        return false;
     }
 
     private static char[,] ReadEngineSchematic()
