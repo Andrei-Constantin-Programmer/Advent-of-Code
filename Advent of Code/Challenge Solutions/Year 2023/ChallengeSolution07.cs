393c1fb35c45f58d87493660736bde82ae534db2
Andrei Constantin <andrei.constantin@outlook.com>
2023-12-07 19:50:11 +0000

    Solved Y2023 D7 P2

--- Advent of Code/Challenge Solutions/Year 2023/ChallengeSolution07.cs
+++ Advent of Code/Challenge Solutions/Year 2023/ChallengeSolution07.cs
@@ -11,7 +11,9 @@ internal class ChallengeSolution07 : ChallengeSolution
         while ((line = read.ReadLine()) != null)
         {
             var elements = line.Split(' ');
-            var cards = elements[0].Select(ParseCard).ToArray();
+            var cards = elements[0]
+                .Select(card => ParseCard(card, includeJokers: false))
+                .ToArray();
             var bid = int.Parse(elements[1]);
 
             hands.Add(new(cards, bid));
@@ -30,10 +32,34 @@ internal class ChallengeSolution07 : ChallengeSolution
 
     protected override void SolveSecondPart()
     {
-        throw new NotImplementedException();
+        List<JokerHand> hands = new();
+
+        string? line;
+        using TextReader read = Reader.GetInputFile(2023, 7);
+        while ((line = read.ReadLine()) != null)
+        {
+            var elements = line.Split(' ');
+            var cards = elements[0]
+                .Select(card => ParseCard(card, includeJokers: true))
+                .ToArray();
+            var bid = int.Parse(elements[1]);
+
+            hands.Add(new(cards, bid));
+        }
+
+        hands = hands.OrderBy(hand => hand).ToList();
+
+        var totalWinnings = 0;
+        for (var rank = 1; rank <= hands.Count; rank++)
+        {
+            totalWinnings += hands[rank - 1].Bid * rank;
+        }
+
+        Console.WriteLine(totalWinnings);
+
     }
 
-    private static Card ParseCard(char character) => character switch
+    private static Card ParseCard(char character, bool includeJokers) => character switch
     {
         '2' => Card.Two,
         '3' => Card.Three,
@@ -44,7 +70,7 @@ internal class ChallengeSolution07 : ChallengeSolution
         '8' => Card.Eight,
         '9' => Card.Nine,
         'T' => Card.T,
-        'J' => Card.J,
+        'J' => includeJokers ? Card.Joker : Card.J,
         'Q' => Card.Q,
         'K' => Card.K,
         'A' => Card.A,
@@ -52,13 +78,78 @@ internal class ChallengeSolution07 : ChallengeSolution
         _ => throw new ArgumentException($"Unknown Card label {character}"),
     };
 
+    private record JokerHand : Hand
+    {
+        public JokerHand(Card[] cards, int bid) : base(cards, bid, GetHandTypeWithJokers(cards)) { }
+
+        protected static HandType GetHandTypeWithJokers(Card[] cards)
+        {
+            var jokerCount = cards.Count(card => card == Card.Joker);
+            if (jokerCount == 0)
+            {
+                return GetHandType(cards);
+            }
+
+            var groupedCards = cards
+                .Where(card => card != Card.Joker)
+                .GroupBy(card => card)
+                .OrderBy(group => group.Count())
+                .ToArray();
+
+            bool isHighCard = groupedCards.Length == cards.Length - jokerCount;
+            if (isHighCard)
+            {
+                return jokerCount switch
+                {
+                    1 => HandType.OnePair,
+                    2 => HandType.ThreeOfAKind,
+                    3 => HandType.FourOfAKind,
+                    4 or 5 => HandType.FiveOfAKind,
+                    _ => HandType.HighCard
+                };
+            }
+
+            bool isOnePair = groupedCards.Length == cards.Length - jokerCount - 1;
+            if (isOnePair)
+            {
+                var highestPair = groupedCards[0].Key;
+                return jokerCount switch
+                {
+                    1 => HandType.ThreeOfAKind,
+                    2 => HandType.FourOfAKind,
+                    3 => HandType.FiveOfAKind,
+                    _ => HandType.HighCard
+                };
+            }
+
+            bool isTwoPairOrThreeOfAKind = groupedCards.Length == cards.Length - jokerCount - 2;
+            if (isTwoPairOrThreeOfAKind)
+            {
+                var isTwoPair = groupedCards.Any(group => group.Count() == 2);
+
+                return isTwoPair
+                    ? HandType.FullHouse
+                    : jokerCount switch
+                    {
+                        1 => HandType.FourOfAKind,
+                        2 => HandType.FiveOfAKind,
+                        _ => HandType.HighCard
+                    };
+            }
+
+            return HandType.FiveOfAKind;
+        }
+    }
+
     private record Hand : IComparable<Hand>
     {
         public Card[] Cards { get; }
         public int Bid { get; }
         public HandType Type { get; }
 
-        public Hand(Card[] cards, int bid)
+        public Hand(Card[] cards, int bid) : this(cards, bid, GetHandType(cards)) { }
+
+        protected Hand(Card[] cards, int bid, HandType type)
         {
             if (cards.Length != 5)
             {
@@ -67,7 +158,7 @@ internal class ChallengeSolution07 : ChallengeSolution
 
             Cards = cards;
             Bid = bid;
-            Type = GetHandType(Cards);
+            Type = type;
         }
 
         public int CompareTo(Hand? other)
@@ -90,7 +181,7 @@ internal class ChallengeSolution07 : ChallengeSolution
             return cardComparison;
         }
 
-        private static HandType GetHandType(Card[] cards)
+        protected static HandType GetHandType(Card[] cards)
         {
             var groupedCards = cards.GroupBy(card => card).ToArray();
 
@@ -117,6 +208,6 @@ internal class ChallengeSolution07 : ChallengeSolution
 
     private enum Card
     {
-        Two, Three, Four, Five, Six, Seven, Eight, Nine, T, J, Q, K, A
+        Joker, Two, Three, Four, Five, Six, Seven, Eight, Nine, T, J, Q, K, A
     }
 }
