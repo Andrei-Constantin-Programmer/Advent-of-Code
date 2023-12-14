b3fef6449e9fedc6508cfc0db00cb22c840534b0
Andrei Constantin <andrei.constantin@outlook.com>
2023-12-14 18:44:14 +0000

    Solved Y2023 D13 P1

---
+++ Advent of Code/Challenge Solutions/Year 2023/ChallengeSolution13.cs
@@ -0,0 +1,103 @@
+﻿using Advent_of_Code.Utilities;
+using System.Text;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2023;
+
+internal class ChallengeSolution13 : ChallengeSolution
+{
+    protected override void SolveFirstPart()
+    {
+        var patterns = ReadPatterns();
+
+        var noteSummary = 0;
+        foreach (var pattern in patterns)
+        {
+            var aboveHorizontalMirror = GetRowsAboveMirror(pattern);
+            var beforeVerticalMirror = aboveHorizontalMirror == 0 ? GetRowsAboveMirror(Transpose(pattern)) : 0;
+
+            noteSummary += beforeVerticalMirror + (100 * aboveHorizontalMirror);
+        }
+
+        Console.WriteLine(noteSummary);
+    }
+
+    protected override void SolveSecondPart()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static int GetRowsAboveMirror(string[] pattern)
+    {
+        var mirrorRow = -1;
+        for (var row = 0; row < pattern.Length - 1 && mirrorRow == -1; row++)
+        {
+            if (IsMirrorRow(pattern, row))
+            {
+                mirrorRow = row;
+            }
+        }
+
+        return mirrorRow >= 0 ? mirrorRow + 1 : 0;
+    }
+
+    private static bool IsMirrorRow(string[] pattern, int mirrorRow)
+    {
+        var isMirror = pattern[mirrorRow] == pattern[mirrorRow + 1];
+
+        if (!isMirror)
+        {
+            return false;
+        }
+
+        for (int rightRow = mirrorRow + 2, leftRow = mirrorRow - 1;
+            rightRow < pattern.Length && leftRow >= 0;
+            rightRow++, leftRow--)
+        {
+            if (pattern[rightRow] != pattern[leftRow])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<string[]> ReadPatterns()
+    {
+        var lines = Reader.ReadLines(this);
+        List<string[]> patterns = new();
+        var emptySpaceIndexes = lines
+            .Select((line, index) => new { line, index })
+            .Where(pair => string.IsNullOrEmpty(pair.line))
+            .Select(pair => pair.index)
+            .ToList();
+
+        for (int index = 0, previousIndex = -1; index <= emptySpaceIndexes.Count; index++, previousIndex++)
+        {
+            var previous = previousIndex >= 0 ? emptySpaceIndexes[previousIndex] + 1 : 0;
+            var current = index < emptySpaceIndexes.Count ? emptySpaceIndexes[index] : lines.Length;
+
+            patterns.Add(lines[previous..current]);
+        }
+
+        return patterns;
+    }
+
+    private static string[] Transpose(string[] pattern)
+    {
+        var transposedPattern = new string[pattern[0].Length];
+
+        for (var col = 0; col < pattern[0].Length; col++)
+        {
+            StringBuilder rowBuilder = new();
+            for (var row = 0; row < pattern.Length; row++)
+            {
+                rowBuilder.Append(pattern[row][col]);
+            }
+
+            transposedPattern[col] = rowBuilder.ToString();
+        }
+
+        return transposedPattern;
+    }
+}
