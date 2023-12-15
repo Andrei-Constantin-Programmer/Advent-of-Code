3616fe5d56399e7233f9f156c1b0fa44cd63f246
Andrei Constantin <andrei.constantin@outlook.com>
2023-12-15 05:08:59 +0000

    Solved Y2023 D15 P1

--- Advent of Code/Challenge Solutions/Year 2023/ChallengeSolution15.cs
+++ Advent of Code/Challenge Solutions/Year 2023/ChallengeSolution15.cs
@@ -6,11 +6,18 @@ internal class ChallengeSolution15 : ChallengeSolution
 {
     protected override void SolveFirstPart()
     {
-        var lines = Reader.ReadLines(this);
+        var stringsToHash = Reader.ReadLines(this)[0].Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+        long sum = stringsToHash.Select(Hash).Sum();
+
+        Console.WriteLine(sum);
     }
 
     protected override void SolveSecondPart()
     {
         throw new NotImplementedException();
     }
+
+    private static int Hash(string stringToHash) => stringToHash
+        .Aggregate(0, (hash, character) => (hash + character) * 17 % 256);
 }
