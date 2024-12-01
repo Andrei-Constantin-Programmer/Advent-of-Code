e99fedb891e4331a5656da057d8f9670d5328487
Andrei Constantin <ucaba98@outlook.com>
2024-12-01 15:03:02 +0000

    Solved Y2024 D1

---
+++ Advent of Code/Challenge Solutions/Year 2023/ChallengeSolution25.cs
@@ -0,0 +1,45 @@
+﻿using Advent_of_Code.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2023;
+
+internal class ChallengeSolution25 : ChallengeSolution
+{
+    private static readonly Random _random = new();
+
+    protected override void SolveFirstPart()
+    {
+        var graph = ReadGraph();
+
+        //var (subgraphSize1, subgraphSize2) = GetPartialGraphSizesAfterDivision(graph);
+
+        //Console.WriteLine(subgraphSize1 * subgraphSize2);
+    }
+
+    protected override void SolveSecondPart()
+    {
+        throw new NotImplementedException();
+    }
+
+    private Dictionary<string, List<string>> ReadGraph()
+    {
+        Dictionary<string, HashSet<string>> graph = new();
+
+        var lines = Reader.ReadLines(this);
+        foreach (var line in lines)
+        {
+            var elements = line.Split(": ");
+            var name = elements[0];
+            var connections = elements[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            graph.TryAdd(name, new());
+            foreach (var connection in connections)
+            {
+                graph[name].Add(connection);
+                graph.TryAdd(connection, new());
+                graph[connection].Add(name);
+            }
+        }
+
+        return graph.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToList());
+    }
+}
---
+++ Advent of Code/Challenge Solutions/Year 2024/ChallengeSolution01.cs
@@ -0,0 +1,54 @@
+﻿using Advent_of_Code.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2024;
+
+public class ChallengeSolution01 : ChallengeSolution
+{
+    protected override void SolveFirstPart()
+    {
+        (var leftList, var rightList) = ReadLocationIds();
+
+        int totalDistance = leftList
+            .Order()
+            .Zip(rightList.Order(), (x, y) => Math.Abs(x - y))
+            .Sum();
+
+        Console.WriteLine($"Total distance: {totalDistance}");
+    }
+
+    protected override void SolveSecondPart()
+    {
+        (var leftList, var rightList) = ReadLocationIds();
+
+        var similarityScore = leftList
+            .Sum(x => x * rightList.Count(y => y == x));
+
+        Console.WriteLine($"Similarity score: {similarityScore}");
+    }
+
+    private (List<int>, List<int>) ReadLocationIds()
+    {
+        List<int> leftList = [];
+        List<int> rightList = [];
+
+        try
+        {
+            foreach (var line in Reader.ReadLines(this))
+            {
+                var values = line
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                leftList.Add(values[0]);
+                rightList.Add(values[1]);
+            }
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Error reading location IDs", e);
+        }
+
+        return (leftList, rightList);
+    }
+}
