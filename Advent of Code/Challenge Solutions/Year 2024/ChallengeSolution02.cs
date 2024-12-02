eb47c180fea3808d6a0ef70543bb20426412cea7
Andrei Constantin <ucaba98@outlook.com>
2024-12-02 11:33:51 +0000

    Solved Y2024 D2 P2

--- Advent of Code/Challenge Solutions/Year 2024/ChallengeSolution02.cs
+++ Advent of Code/Challenge Solutions/Year 2024/ChallengeSolution02.cs
@@ -11,32 +11,7 @@ public class ChallengeSolution02 : ChallengeSolution
 
         foreach (var report in reports)
         {
-            var isSafe = true;
-            var isIncreasing = report[0] < report[1];
-
-            for (var level = 0; level < report.Count - 1; level++)
-            {
-                if (isIncreasing && report[level] > report[level + 1])
-                {
-                    isSafe = false;
-                    break;
-                }
-
-                if (!isIncreasing && report[level] < report[level + 1])
-                {
-                    isSafe = false;
-                    break;
-                }
-
-                var difference = Math.Abs(report[level] - report[level + 1]);
-                if (difference is 0 or > 3)
-                {
-                    isSafe = false;
-                    break;
-                }
-            }
-
-            if (isSafe)
+            if (IsSafeReport(report))
             {
                 safeReports++;
             }
@@ -47,7 +22,87 @@ public class ChallengeSolution02 : ChallengeSolution
 
     protected override void SolveSecondPart()
     {
-        throw new NotImplementedException();
+        var reports = ReadReports();
+        var safeReports = 0;
+
+        foreach (var report in reports)
+        {
+            if (IsSafeReport(report))
+            {
+                safeReports++;
+            }
+            else
+            {
+                for (var level = 0; level < report.Count; level++)
+                {
+                    if (IsSafeReport(report, level))
+                    {
+                        safeReports++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine($"Safe reports: {safeReports}");
+    }
+
+    private static bool IsSafeReport(List<int> report, int? skipIndex = null)
+    {
+        var isSafe = true;
+
+        var isIncreasing =
+            skipIndex is 0 ? report[1] < report[2] :
+            skipIndex is 1 ? report[0] < report[2] :
+                             report[0] < report[1];
+
+        for (var level = 0; level < report.Count - 1; level++)
+        {
+            if (skipIndex == level)
+            {
+                continue;
+            }
+
+            if (skipIndex == level + 1)
+            {
+                if (level + 2 >= report.Count)
+                {
+                    continue;
+                }
+
+                if (IsUnsafeLevelDifference(isIncreasing, report[level], report[level + 2]))
+                {
+                    isSafe = false;
+                    break;
+                }
+
+                continue;
+            }
+
+            if (IsUnsafeLevelDifference(isIncreasing, report[level], report[level + 1]))
+            {
+                isSafe = false;
+                break;
+            }
+        }
+
+        return isSafe;
+    }
+
+    private static bool IsUnsafeLevelDifference(bool isIncreasing, int level1, int level2)
+    {
+        if (isIncreasing && level1 > level2)
+        {
+            return true;
+        }
+
+        if (!isIncreasing && level1 < level2)
+        {
+            return true;
+        }
+
+        var difference = Math.Abs(level1 - level2);
+        return difference is 0 or > 3;
     }
 
     private List<List<int>> ReadReports()
