d3a543c1fd3d6f7a64d52419ffb473face53a440
Andrei Constantin <ucaba98@outlook.com>
2024-12-03 05:29:04 +0000

    Solved Y2024 D3 P2

--- Advent of Code/Challenge Solutions/Year 2024/ChallengeSolution03.cs
+++ Advent of Code/Challenge Solutions/Year 2024/ChallengeSolution03.cs
@@ -16,15 +16,8 @@ public partial class ChallengeSolution03 : ChallengeSolution
         var sum = 0;
         foreach (var mul in muls)
         {
-            var leftParenthesis = mul.IndexOf('(');
-            var rightParenthesis = mul.IndexOf(')');
-
-            var values = mul[(leftParenthesis + 1)..rightParenthesis]
-                .Split(',')
-                .Select(int.Parse)
-                .ToArray();
-
-            sum += values[0] * values[1];
+            var (x, y) = ConvertMul(mul);
+            sum += x * y;
         }
 
         Console.WriteLine($"Multiplication sum: {sum}");
@@ -32,13 +25,58 @@ public partial class ChallengeSolution03 : ChallengeSolution
 
     protected override void SolveSecondPart()
     {
+        var lines = ReadMemory();
+        var instructions = InstructionRegex()
+            .Matches(lines)
+            .Select(match => match.Value)
+            .ToList();
 
+        var doMultiplication = true;
+        var sum = 0;
+
+        foreach (var instruction in instructions)
+        {
+            if (instruction == "do()")
+            {
+                doMultiplication = true;
+                continue;
+            }
+
+            if (instruction == "don't()")
+            {
+                doMultiplication = false;
+                continue;
+            }
+
+            if (doMultiplication)
+            {
+                var (x, y) = ConvertMul(instruction);
+                sum += x * y;
+            }
+        }
+
+        Console.WriteLine($"Multiplication sum: {sum}");
+    }
+
+    private static (int, int) ConvertMul(string mul)
+    {
+        var leftParenthesis = mul.IndexOf('(');
+        var rightParenthesis = mul.IndexOf(')');
+        var values = mul[(leftParenthesis + 1)..rightParenthesis]
+            .Split(',')
+            .Select(int.Parse)
+            .ToArray();
+
+        return (values[0], values[1]);
     }
 
     public string ReadMemory()
     {
         return string.Join("", Reader.ReadLines(this));
     }
+
+    [GeneratedRegex(@"mul\(\d+,\d+\)|do\(\)|don't\(\)")]
+    private static partial Regex InstructionRegex();
 
     [GeneratedRegex(@"mul\(\d+,\d+\)")]
     private static partial Regex MulRegex();
