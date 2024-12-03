0d2dc828286dda041a12a2ac5e67e79670cbdb8e
Andrei Constantin <ucaba98@outlook.com>
2024-12-03 05:17:03 +0000

    Solved Y2024 D3 P1

---
+++ Advent of Code/Challenge Solutions/Year 2024/ChallengeSolution03.cs
@@ -0,0 +1,45 @@
+﻿using Advent_of_Code.Utilities;
+using System.Text.RegularExpressions;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2024;
+
+public partial class ChallengeSolution03 : ChallengeSolution
+{
+    protected override void SolveFirstPart()
+    {
+        var lines = ReadMemory();
+        var muls = MulRegex()
+            .Matches(lines)
+            .Select(match => match.Value)
+            .ToList();
+
+        var sum = 0;
+        foreach (var mul in muls)
+        {
+            var leftParenthesis = mul.IndexOf('(');
+            var rightParenthesis = mul.IndexOf(')');
+
+            var values = mul[(leftParenthesis + 1)..rightParenthesis]
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+
+            sum += values[0] * values[1];
+        }
+
+        Console.WriteLine($"Multiplication sum: {sum}");
+    }
+
+    protected override void SolveSecondPart()
+    {
+
+    }
+
+    public string ReadMemory()
+    {
+        return string.Join("", Reader.ReadLines(this));
+    }
+
+    [GeneratedRegex(@"mul\(\d+,\d+\)")]
+    private static partial Regex MulRegex();
+}
