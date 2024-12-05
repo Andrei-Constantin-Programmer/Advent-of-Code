44e58adc7ea80280db41585ca04780224f8b4027
Andrei Constantin <andrei.constantin@outlook.com>
2024-12-05 18:22:36 +0000

    Implemented input parsing for Y2024 D5

---
+++ Advent of Code.Tests/Challenge Solutions/Year 2024/ChallengeSolutionTests05.cs
@@ -0,0 +1,104 @@
+﻿using Advent_of_Code.Challenge_Solutions.Year_2024;
+using Advent_of_Code.Utilities;
+using NSubstitute;
+
+namespace Advent_of_Code.Tests.Challenge_Solutions.Year_2024;
+
+public class ChallengeSolutionTests05
+{
+    protected IConsole _consoleMock;
+    protected ISolutionReader<ChallengeSolution05> _readerMock;
+
+    private readonly ChallengeSolution05 _challengeSolution;
+
+    private readonly string[] _sampleInput =
+    [
+        "47|53",
+        "97|13",
+        "97|61",
+        "97|47",
+        "75|29",
+        "61|13",
+        "75|53",
+        "29|13",
+        "97|29",
+        "53|29",
+        "61|53",
+        "97|53",
+        "61|29",
+        "47|13",
+        "75|47",
+        "97|75",
+        "47|61",
+        "75|61",
+        "47|29",
+        "75|13",
+        "53|13",
+        "",
+        "75,47,61,53,29",
+        "97,61,53,29,13",
+        "75,29,13",
+        "75,97,47,61,53",
+        "61,13,29",
+        "97,13,75,29,47",
+    ];
+
+    public ChallengeSolutionTests05()
+    {
+        _consoleMock = Substitute.For<IConsole>();
+        _readerMock = Substitute.For<ISolutionReader<ChallengeSolution05>>();
+
+        _readerMock.ReadLines()
+            .Returns(TestHelpers.GetInputFileContents(2024, 5));
+
+        _challengeSolution = new ChallengeSolution05(_consoleMock, _readerMock);
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(143)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(2567)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(9)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(2029)));
+    }
+}
---
+++ Advent of Code/Challenge Solutions/Year 2024/ChallengeSolution05.cs
@@ -0,0 +1,58 @@
+﻿// Task: https://adventofcode.com/2024/day/5
+
+using Advent_of_Code.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2024;
+
+public class ChallengeSolution05 : ChallengeSolution<ChallengeSolution05>
+{
+    private readonly Dictionary<int, List<int>> _rules;
+    private readonly List<int[]> _updates;
+
+    public ChallengeSolution05(IConsole console, ISolutionReader<ChallengeSolution05> reader) : base(console, reader)
+    {
+        var lines = reader.ReadLines();
+        var emptyLineIndex = Array.FindIndex(lines, string.IsNullOrEmpty);
+        var (rulesSection, updatesSection) = (lines.Take(emptyLineIndex).ToArray(), lines.Skip(emptyLineIndex + 1).ToArray());
+
+        _rules = GetRules(rulesSection);
+        _updates = GetUpdates(updatesSection);
+    }
+
+    public override void SolveFirstPart()
+    {
+
+    }
+
+    public override void SolveSecondPart()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Dictionary<int, List<int>> GetRules(string[] pageOrderingRules)
+    {
+        Dictionary<int, List<int>> pageOrderings = [];
+
+        foreach (var pageOrderingRule in pageOrderingRules)
+        {
+            var pageNumbers = pageOrderingRule.Split('|').Select(int.Parse).ToArray();
+            if (!pageOrderings.TryGetValue(pageNumbers[0], out List<int>? pagesAfter))
+            {
+                pageOrderings.Add(pageNumbers[0], [pageNumbers[1]]);
+            }
+            else
+            {
+                pagesAfter.Add(pageNumbers[1]);
+            }
+        }
+
+        return pageOrderings;
+    }
+
+    private static List<int[]> GetUpdates(string[] updates) => updates
+        .Select(update => update
+            .Split(',')
+            .Select(int.Parse)
+            .ToArray())
+        .ToList();
+}
