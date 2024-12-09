8fa9cbe5325aa99fc53241a81530a10430c39f78
Andrei Constantin <andrei.constantin@outlook.com>
2024-12-09 17:34:42 +0000

    Solved Y2024 D9 P1

---
+++ Advent of Code.Tests/Challenge Solutions/Year 2024/ChallengeSolutionTests09.cs
@@ -0,0 +1,103 @@
+﻿using Advent_of_Code.Challenge_Solutions.Year_2024;
+using Advent_of_Code.Utilities;
+using NSubstitute;
+
+namespace Advent_of_Code.Tests.Challenge_Solutions.Year_2024;
+
+public class ChallengeSolutionTests09
+{
+    protected IConsole _consoleMock;
+    protected ISolutionReader<ChallengeSolution09> _readerMock;
+
+    private readonly ChallengeSolution09 _challengeSolution;
+
+    private readonly string[] _sampleInputSmall = ["12345"];
+    private readonly string[] _sampleInputMedium = ["2333133121414131402"];
+
+    public ChallengeSolutionTests09()
+    {
+        _consoleMock = Substitute.For<IConsole>();
+        _readerMock = Substitute.For<ISolutionReader<ChallengeSolution09>>();
+
+        _readerMock.ReadLines()
+            .Returns(TestHelpers.GetInputFileContents(2024, 9));
+
+        _challengeSolution = new ChallengeSolution09(_consoleMock, _readerMock);
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInputSmall()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInputSmall);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(60)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInputMedium()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInputMedium);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(1928)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(6320029754031)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInputSmall()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInputSmall);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(34)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInputMedium()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInputMedium);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(34)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(1017)));
+    }
+}
---
+++ Advent of Code/Challenge Solutions/Year 2024/ChallengeSolution09.cs
@@ -0,0 +1,91 @@
+﻿// Task: https://adventofcode.com/2024/day/9
+
+using Advent_of_Code.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2024;
+
+public class ChallengeSolution09(IConsole console, ISolutionReader<ChallengeSolution09> reader)
+    : ChallengeSolution<ChallengeSolution09>(console, reader)
+{
+    private const int FREE_SPACE = -1;
+
+    public override void SolveFirstPart()
+    {
+        var diskMap = ReadDiskMap();
+        var blocks = GenerateBlocks(diskMap);
+
+        CompactBlocks(blocks);
+
+        long checksum = 0;
+        for (var blockId = 0; blocks[blockId] != FREE_SPACE; blockId++)
+        {
+            checksum += blockId * (long)blocks[blockId];
+        }
+
+        _console.WriteLine($"Checksum: {checksum}");
+    }
+
+    private static void CompactBlocks(int[] blocks)
+    {
+        var leftIndex = 0;
+        var rightIndex = blocks.Length - 1;
+
+        while (leftIndex < rightIndex)
+        {
+            if (blocks[leftIndex] == FREE_SPACE)
+            {
+                while (blocks[rightIndex] == FREE_SPACE)
+                {
+                    rightIndex--;
+                }
+
+                blocks[leftIndex] = blocks[rightIndex];
+                blocks[rightIndex--] = FREE_SPACE;
+            }
+
+            leftIndex++;
+        }
+    }
+
+    private static int[] GenerateBlocks(byte[] diskMap)
+    {
+        var blocks = Enumerable
+                    .Repeat(FREE_SPACE, diskMap.Length * 9)
+                    .ToArray();
+
+        var id = 0;
+        var blockId = 0;
+
+        for (var diskMapIndex = 0; diskMapIndex < diskMap.Length; diskMapIndex++)
+        {
+            if (diskMapIndex % 2 == 0)
+            {
+                for (var i = 0; i < diskMap[diskMapIndex]; i++)
+                {
+                    blocks[blockId++] = id;
+                }
+
+                id++;
+            }
+            else
+            {
+                blockId += diskMap[diskMapIndex];
+            }
+        }
+
+        return blocks;
+    }
+
+    public override void SolveSecondPart()
+    {
+        throw new NotImplementedException();
+    }
+
+    private byte[] ReadDiskMap()
+    {
+        var lines = _reader.ReadLines();
+        return lines[0]
+            .Select(c => (byte)(c - '0'))
+            .ToArray();
+    }
+}
