c2ddf2be272abf4a6d67f6983941b3e9e7f6be18
Andrei Constantin <andrei.constantin@outlook.com>
2024-12-11 05:49:03 +0000

    Solved Y2024 D11 P1

---
+++ Advent of Code.Tests/Challenge Solutions/Year 2024/ChallengeSolutionTests11.cs
@@ -0,0 +1,77 @@
+﻿using Advent_of_Code.Challenge_Solutions.Year_2024;
+using Advent_of_Code.Utilities;
+using NSubstitute;
+
+namespace Advent_of_Code.Tests.Challenge_Solutions.Year_2024;
+
+public class ChallengeSolutionTests11
+{
+    protected IConsole _consoleMock;
+    protected ISolutionReader<ChallengeSolution11> _readerMock;
+
+    private readonly ChallengeSolution11 _challengeSolution;
+
+    private readonly string[] _sampleInput =
+    [
+        "125 17"
+    ];
+
+    public ChallengeSolutionTests11()
+    {
+        _consoleMock = Substitute.For<IConsole>();
+        _readerMock = Substitute.For<ISolutionReader<ChallengeSolution11>>();
+
+        _readerMock.ReadLines()
+            .Returns(TestHelpers.GetInputFileContents(2024, 11));
+
+        _challengeSolution = new ChallengeSolution11(_consoleMock, _readerMock);
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(55312)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(203228)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(81)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(1706)));
+    }
+}
---
+++ Advent of Code/Challenge Solutions/Year 2024/ChallengeSolution11.cs
@@ -0,0 +1,99 @@
+﻿// Task: https://adventofcode.com/2024/day/11
+
+using Advent_of_Code.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2024;
+
+public class ChallengeSolution11(IConsole console, ISolutionReader<ChallengeSolution11> reader)
+    : ChallengeSolution<ChallengeSolution11>(console, reader)
+{
+    public override void SolveFirstPart()
+    {
+        SolveStoneProblem(25);
+    }
+
+    public override void SolveSecondPart()
+    {
+        SolveStoneProblem(75);
+    }
+
+    private void SolveStoneProblem(int blinks)
+    {
+        var stones = new Queue<long>(ReadStones());
+        Dictionary<long, List<long>> stoneStore = [];
+
+        for (var blink = 0; blink < blinks; blink++)
+        {
+            var stoneCount = stones.Count;
+            ComputeNextStoneSet(stones, stoneStore, stoneCount);
+        }
+
+        _console.WriteLine(stones.Count);
+    }
+
+    private static void ComputeNextStoneSet(Queue<long> stones, Dictionary<long, List<long>> stoneStore, int stoneCount)
+    {
+        for (var i = 0; i < stoneCount; i++)
+        {
+            var stone = stones.Dequeue();
+
+            if (stoneStore.TryGetValue(stone, out var results))
+            {
+                foreach (var result in results)
+                {
+                    stones.Enqueue(result);
+                }
+
+                continue;
+            }
+
+            var stoneString = stone.ToString();
+            var stoneLength = stoneString.Length;
+            List<long> newStones = FindNewStones(stone, stoneString, stoneLength);
+
+            foreach (var newStone in newStones)
+            {
+                stones.Enqueue(newStone);
+            }
+
+            stoneStore[stone] = newStones;
+        }
+    }
+
+    private static List<long> FindNewStones(long stone, string stoneString, int stoneLength)
+    {
+        List<long> newResults = [];
+
+        if (stone == 0)
+        {
+            newResults.Add(1);
+        }
+        else if (stoneLength % 2 == 0)
+        {
+            var (firstHalf, secondHalf) = SplitStone(stoneString, stoneLength);
+
+            newResults.Add(firstHalf);
+            newResults.Add(secondHalf);
+        }
+        else
+        {
+            newResults.Add(stone * 2024);
+        }
+
+        return newResults;
+    }
+
+    private static (long, long) SplitStone(string stoneString, int stoneLength)
+    {
+        var middle = stoneLength / 2;
+        var firstHalf = long.Parse(stoneString[..middle]);
+        var secondHalf = long.Parse(stoneString[middle..]);
+
+        return (firstHalf, secondHalf);
+    }
+
+    private List<long> ReadStones() => _reader.ReadLines()[0]
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(long.Parse)
+        .ToList();
+}
