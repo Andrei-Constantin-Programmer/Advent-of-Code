7955aa7f3fd4346deb2617a683154b27befe0e01
Andrei Constantin <andrei.constantin@outlook.com>
2024-12-12 21:46:23 +0000

    Moved resources in submodule for privacy

--- Advent of Code/Utilities/PathUtils.cs
+++ Advent of Code/Utilities/PathUtils.cs
@@ -5,7 +5,8 @@ public static class PathUtils
     public static StreamWriter GetOutputFile(int year, int day) => new(GetFilePath(FileType.Output, year, day));
 
     public static string GetFilePath(FileType fileType, int year, int day) =>
-        Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.FullName + @$"\resources\{fileType.ToString().ToLower()}\{year}\{FormatDay(day)}.txt";
+        Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.Parent!.FullName
+            + @$"\resources\{fileType.ToString().ToLower()}\{year}\{FormatDay(day)}.txt";
 
     public static string FormatDay(int day) => day < 10 ? $"0{day}" : $"{day}";
 
