45ae49e5ff67c2a70559c3b59fb7fd9052cc26bb
Andrei Constantin <andrei.constantin@outlook.com>
2024-12-12 06:25:27 +0000

    Solved Y2024 D12 P1

---
+++ Advent of Code.Tests/Challenge Solutions/Year 2024/ChallengeSolutionTests12.cs
@@ -0,0 +1,159 @@
+﻿using Advent_of_Code.Challenge_Solutions.Year_2024;
+using Advent_of_Code.Utilities;
+using NSubstitute;
+
+namespace Advent_of_Code.Tests.Challenge_Solutions.Year_2024;
+
+public class ChallengeSolutionTests12
+{
+    protected IConsole _consoleMock;
+    protected ISolutionReader<ChallengeSolution12> _readerMock;
+
+    private readonly ChallengeSolution12 _challengeSolution;
+
+    private readonly string[] _sampleInputSmall =
+    [
+        "AAAA",
+        "BBCD",
+        "BBCC",
+        "EEEC",
+    ];
+
+    private readonly string[] _sampleInputMedium =
+    [
+        "OOOOO",
+        "OXOXO",
+        "OOOOO",
+        "OXOXO",
+        "OOOOO",
+    ];
+
+    private readonly string[] _sampleInputLarge =
+    [
+        "RRRRIICCFF",
+        "RRRRIICCCF",
+        "VVRRRCCFFF",
+        "VVRCCCJFFF",
+        "VVVVCJJCFE",
+        "VVIVCCJJEE",
+        "VVIIICJJEE",
+        "MIIIIIJJEE",
+        "MIIISIJEEE",
+        "MMMISSJEEE",
+    ];
+
+    public ChallengeSolutionTests12()
+    {
+        _consoleMock = Substitute.For<IConsole>();
+        _readerMock = Substitute.For<ISolutionReader<ChallengeSolution12>>();
+
+        _readerMock.ReadLines()
+            .Returns(TestHelpers.GetInputFileContents(2024, 12));
+
+        _challengeSolution = new ChallengeSolution12(_consoleMock, _readerMock);
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInputSmall()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInputSmall);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(140)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInputMedium()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInputMedium);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(772)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInputLarge()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInputLarge);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(1930)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(0)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInputSmall()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInputSmall);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(0)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInputMedium()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInputMedium);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(0)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleLarge()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInputLarge);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(0)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(0)));
+    }
+}
---
+++ Advent of Code/Challenge Solutions/Year 2024/ChallengeSolution12.cs
@@ -0,0 +1,134 @@
+﻿// Task: https://adventofcode.com/2024/day/12
+
+using Advent_of_Code.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2024;
+
+public class ChallengeSolution12(IConsole console, ISolutionReader<ChallengeSolution12> reader)
+    : ChallengeSolution<ChallengeSolution12>(console, reader)
+{
+    private static Point[] Directions =>
+    [
+        new(-1, 0),
+        new (1, 0),
+        new (0, -1),
+        new (0, 1),
+    ];
+
+    public override void SolveFirstPart()
+    {
+        var plotMap = ReadGardenPlotMap();
+        List<long> plotPrices = [];
+        HashSet<Point> visited = [];
+
+        long totalPrice = 0;
+
+        for (var row = 0; row < plotMap.Length; row++)
+        {
+            for (var col = 0; col < plotMap[row].Length; col++)
+            {
+                Point point = new(row, col);
+                if (visited.Contains(point))
+                {
+                    continue;
+                }
+
+                var price = FindPriceForPlotStartingAt(plotMap, point, out var newlyVisited);
+                totalPrice += price;
+
+                visited.UnionWith(newlyVisited);
+            }
+        }
+
+        _console.WriteLine($"Total Cost: {totalPrice}");
+    }
+
+    private long FindPriceForPlotStartingAt(string[] plotMap, Point point, out HashSet<Point> visited)
+    {
+        var plot = GetPlot(plotMap, point, out visited);
+
+        var area = plot.Count;
+        var perimeter = CalculatePerimeter(plotMap, plot);
+
+        return area * perimeter;
+    }
+
+    private int CalculatePerimeter(string[] plotMap, List<Point> plot)
+    {
+        var perimeter = 0;
+
+        var plant = plotMap[plot[0].Row][plot[0].Col];
+        foreach (var point in plot)
+        {
+            foreach (var direction in Directions)
+            {
+                Point neighbour = new(point.Row + direction.Row, point.Col + direction.Col);
+
+                if (IsOutOfBounds(plotMap, neighbour)
+                    || plotMap[neighbour.Row][neighbour.Col] != plant)
+                {
+                    perimeter++;
+                }
+            }
+        }
+
+        return perimeter;
+    }
+
+    private List<Point> GetPlot(string[] plotMap, Point start, out HashSet<Point> visited)
+    {
+        List<Point> result = [];
+        visited = [];
+
+        var plant = plotMap[start.Row][start.Col];
+
+        Queue<Point> queue = [];
+        queue.Enqueue(start);
+        visited.Add(start);
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            result.Add(current);
+
+            foreach (var direction in Directions)
+            {
+                Point nextPoint = new(current.Row + direction.Row, current.Col + direction.Col);
+
+                if (IsOutOfBounds(plotMap, nextPoint))
+                {
+                    continue;
+                }
+
+                if (visited.Contains(nextPoint)
+                    || plotMap[nextPoint.Row][nextPoint.Col] != plant)
+                {
+                    continue;
+                }
+
+                queue.Enqueue(nextPoint);
+                visited.Add(nextPoint);
+            }
+        }
+
+        return result;
+    }
+
+    private static bool IsOutOfBounds(string[] plotMap, Point point)
+        => point.Row < 0
+        || point.Row >= plotMap.Length
+        || point.Col < 0
+        || point.Col >= plotMap[point.Row].Length;
+
+    public override void SolveSecondPart()
+    {
+        throw new NotImplementedException();
+    }
+
+    private string[] ReadGardenPlotMap()
+    {
+        return _reader.ReadLines();
+    }
+
+    private record Point(int Row, int Col);
+}
