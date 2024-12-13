b08281bab496a0878dff2710848e78f1e693f2ab
Andrei Constantin <andrei.constantin@outlook.com>
2024-12-13 06:11:48 +0000

    Solved Y2024 D13 P1

---
+++ Advent of Code.Tests/Challenge Solutions/Year 2024/ChallengeSolutionTests13.cs
@@ -0,0 +1,91 @@
+﻿using Advent_of_Code.Challenge_Solutions.Year_2024;
+using Advent_of_Code.Utilities;
+using NSubstitute;
+
+namespace Advent_of_Code.Tests.Challenge_Solutions.Year_2024;
+
+public class ChallengeSolutionTests13
+{
+    protected IConsole _consoleMock;
+    protected ISolutionReader<ChallengeSolution13> _readerMock;
+
+    private readonly ChallengeSolution13 _challengeSolution;
+
+    private readonly string[] _sampleInput =
+    [
+        "Button A: X+94, Y+34",
+        "Button B: X+22, Y+67",
+        "Prize: X=8400, Y=5400",
+        "",
+        "Button A: X+26, Y+66",
+        "Button B: X+67, Y+21",
+        "Prize: X=12748, Y=12176",
+        "",
+        "Button A: X+17, Y+86",
+        "Button B: X+84, Y+37",
+        "Prize: X=7870, Y=6450",
+        "",
+        "Button A: X+69, Y+23",
+        "Button B: X+27, Y+71",
+        "Prize: X=18641, Y=10279",
+    ];
+
+    public ChallengeSolutionTests13()
+    {
+        _consoleMock = Substitute.For<IConsole>();
+        _readerMock = Substitute.For<ISolutionReader<ChallengeSolution13>>();
+
+        _readerMock.ReadLines()
+            .Returns(TestHelpers.GetInputFileContents(2024, 13));
+
+        _challengeSolution = new ChallengeSolution13(_consoleMock, _readerMock);
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(480)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(37297)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(65601038650482)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(240884656550923)));
+    }
+}
---
+++ Advent of Code/Challenge Solutions/Year 2024/ChallengeSolution13.cs
@@ -0,0 +1,136 @@
+﻿// Task: https://adventofcode.com/2024/day/13
+
+using Advent_of_Code.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2024;
+
+public class ChallengeSolution13(IConsole console, ISolutionReader<ChallengeSolution13> reader)
+    : ChallengeSolution<ChallengeSolution13>(console, reader)
+{
+    const int ACost = 3;
+    const int BCost = 1;
+    const int MaxPresses = 100;
+
+    public override void SolveFirstPart()
+    {
+        var machines = ReadMachines();
+        var totalTokens = ComputeTotalTokens(machines);
+
+        _console.WriteLine($"Tokens: {totalTokens}");
+    }
+
+    public override void SolveSecondPart()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static int ComputeTotalTokens(List<Machine> machines)
+    {
+        var totalTokens = 0;
+        foreach (var machine in machines)
+        {
+            var (aPresses, bPresses) = FindMinimalPressesRequired(machine);
+
+            if (aPresses > MaxPresses
+                || bPresses > MaxPresses)
+            {
+                continue;
+            }
+
+            totalTokens += (aPresses * ACost) + (bPresses * BCost);
+        }
+
+        return totalTokens;
+    }
+
+    private static (int aPresses, int bPresses) FindMinimalPressesRequired(Machine machine)
+    {
+        var aPresses = 0;
+        var bPresses = 0;
+        var minCost = int.MaxValue;
+
+        for (var a = 0; a * machine.A.X <= machine.Prize.X; a++)
+        {
+            var remainingX = machine.Prize.X - (a * machine.A.X);
+            if (remainingX % machine.B.X == 0)
+            {
+                var bX = remainingX / machine.B.X;
+
+                var totalY = (a * machine.A.Y) + (bX * machine.B.Y);
+                if (totalY == machine.Prize.Y)
+                {
+                    var currentCost = (a * ACost) + (bX * BCost);
+
+                    if (currentCost < minCost)
+                    {
+                        aPresses = a;
+                        bPresses = bX;
+                        minCost = currentCost;
+                    }
+                }
+            }
+        }
+
+        return (aPresses, bPresses);
+    }
+
+    private static (int gcd, int aCoefficient, int bCoefficient) GreatestCommonDivisorWithCoefficients(int a, int b)
+    {
+        if (b == 0)
+        {
+            return (a, 1, 0);
+        }
+
+        var (gcd, aCoeff, bCoeff) = GreatestCommonDivisorWithCoefficients(b, a % b);
+        var x = bCoeff;
+        var y = aCoeff - (a / b * bCoeff);
+
+        return (gcd, x, y);
+    }
+
+
+    private List<Machine> ReadMachines()
+    {
+        List<Machine> machines = [];
+        var lines = _reader.ReadLines();
+
+        for (var i = 0; i < lines.Length; i++)
+        {
+            var pointA = ExtractPoint(lines[i++]);
+            var pointB = ExtractPoint(lines[i++]);
+            var prize = ExtractPoint(lines[i++]);
+
+            Machine machine = new(pointA, pointB, prize);
+            machines.Add(machine);
+        }
+
+        return machines;
+    }
+
+    private static Point ExtractPoint(string input)
+    {
+        var xPlus = input.IndexOf('+');
+
+        if (xPlus >= 0)
+        {
+            var yPlus = input.IndexOf('+', xPlus + 1);
+
+            return new Point(
+                int.Parse(input[(xPlus + 1)..input.IndexOf(',')]),
+                int.Parse(input[(yPlus + 1)..])
+            );
+        }
+
+        var xEqual = input.IndexOf('=');
+        var yEqual = input.IndexOf('=', xEqual + 1);
+
+        return new Point(
+                int.Parse(input[(xEqual + 1)..input.IndexOf(',')]),
+                int.Parse(input[(yEqual + 1)..])
+            );
+    }
+
+    private record Machine(Point A, Point B, Point Prize);
+
+    private record struct Point(int X, int Y);
+}
