2de68caa3e15dae3132383ae4c526e2a5bd5a45f
Andrei Constantin <andrei.constantin@outlook.com>
2024-12-15 13:55:18 +0000

    Solved Y2024 D15 P1

---
+++ Advent of Code.Tests/Challenge Solutions/Year 2024/ChallengeSolutionTests15.cs
@@ -0,0 +1,97 @@
+﻿using Advent_of_Code.Challenge_Solutions.Year_2024;
+using Advent_of_Code.Utilities;
+using NSubstitute;
+
+namespace Advent_of_Code.Tests.Challenge_Solutions.Year_2024;
+
+public class ChallengeSolutionTests15
+{
+    protected IConsole _consoleMock;
+    protected ISolutionReader<ChallengeSolution15> _readerMock;
+
+    private readonly ChallengeSolution15 _challengeSolution;
+
+    private readonly string[] _sampleInput =
+    [
+        "##########",
+        "#..O..O.O#",
+        "#......O.#",
+        "#.OO..O.O#",
+        "#..O@..O.#",
+        "#O#..O...#",
+        "#O..O..O.#",
+        "#.OO.O.OO#",
+        "#....O...#",
+        "##########",
+        "",
+        "<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+        "vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+        "><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+        "<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+        "^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+        "^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+        ">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+        "<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+        "^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+        "v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+    ];
+
+    public ChallengeSolutionTests15()
+    {
+        _consoleMock = Substitute.For<IConsole>();
+        _readerMock = Substitute.For<ISolutionReader<ChallengeSolution15>>();
+
+        _readerMock.ReadLines()
+            .Returns(TestHelpers.GetInputFileContents(2024, 15));
+
+        _challengeSolution = new ChallengeSolution15(_consoleMock, _readerMock);
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(10092)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(1398947)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(9021)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(0)));
+    }
+}
---
+++ Advent of Code/Challenge Solutions/Year 2024/ChallengeSolution15.cs
@@ -0,0 +1,176 @@
+﻿// Task: https://adventofcode.com/2024/day/15
+
+using Advent_of_Code.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2024;
+
+public class ChallengeSolution15(IConsole console, ISolutionReader<ChallengeSolution15> reader)
+    : ChallengeSolution<ChallengeSolution15>(console, reader)
+{
+    private const char Wall = '#';
+    private const char Box = 'O';
+    private const char Robot = '@';
+    private const char Empty = '.';
+
+    private const int GpsRowMultiplier = 100;
+
+    private static readonly Dictionary<Direction, Point> _directionChanges = new()
+    {
+        { Direction.Up,     new Point(-1, 0) },
+        { Direction.Right,  new Point(0, 1) },
+        { Direction.Down,   new Point(1, 0) },
+        { Direction.Left,   new Point(0, -1) }
+    };
+
+    public override void SolveFirstPart()
+    {
+        var (warehouseMap, directions) = ReadWarehouseInformation(out var robotPosition);
+        PerformMoveOperations(warehouseMap, directions, robotPosition);
+
+        PrintMap(warehouseMap);
+        var gpsCoordinatesSum = FindBoxGpsCoordinatesSum(warehouseMap);
+
+        _console.WriteLine($"GPS Box Coordinate Sum: {gpsCoordinatesSum}");
+    }
+
+    public override void SolveSecondPart()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void PerformMoveOperations(char[][] warehouseMap, List<Direction> directions, Point robotPosition)
+    {
+        foreach (var direction in directions)
+        {
+            var directionChange = _directionChanges[direction];
+            var newRobotPosition = Move(robotPosition, directionChange);
+
+            if (warehouseMap[newRobotPosition.Row][newRobotPosition.Col] is Empty)
+            {
+                warehouseMap[newRobotPosition.Row][newRobotPosition.Col] = Robot;
+                warehouseMap[robotPosition.Row][robotPosition.Col] = Empty;
+                robotPosition = newRobotPosition;
+                continue;
+            }
+
+            if (warehouseMap[newRobotPosition.Row][newRobotPosition.Col] is Wall
+                || warehouseMap[newRobotPosition.Row][newRobotPosition.Col] is not Box)
+            {
+                continue;
+            }
+
+            var boxPosition = newRobotPosition;
+            Point? emptyPoint = FindNextEmptyPoint(warehouseMap, directionChange, boxPosition);
+
+            if (emptyPoint is null)
+            {
+                continue;
+            }
+
+            warehouseMap[emptyPoint.Row][emptyPoint.Col] = Box;
+            warehouseMap[newRobotPosition.Row][newRobotPosition.Col] = Robot;
+            warehouseMap[robotPosition.Row][robotPosition.Col] = Empty;
+            robotPosition = newRobotPosition;
+        }
+    }
+
+    private static Point? FindNextEmptyPoint(char[][] warehouseMap, Point directionChange, Point boxPosition)
+    {
+        Point? emptyPoint = null;
+
+        while (emptyPoint is null)
+        {
+            boxPosition = Move(boxPosition, directionChange);
+            if (warehouseMap[boxPosition.Row][boxPosition.Col] is Wall)
+            {
+                return null;
+            }
+
+            if (warehouseMap[boxPosition.Row][boxPosition.Col] is Empty)
+            {
+                emptyPoint = boxPosition;
+            }
+        }
+
+        return emptyPoint;
+    }
+
+    private static int FindBoxGpsCoordinatesSum(char[][] warehouseMap)
+    {
+        var sum = 0;
+
+        for (var i = 1; i < warehouseMap.Length - 1; i++)
+        {
+            for (var j = 1; j < warehouseMap[i].Length - 1; j++)
+            {
+                if (warehouseMap[i][j] is Box)
+                {
+                    sum += GpsRowMultiplier * i + j;
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    private void PrintMap(char[][] warehouseMap)
+    {
+        for (var i = 0; i < warehouseMap.Length; i++)
+        {
+            for (var j = 0; j < warehouseMap[i].Length; j++)
+            {
+                _console.Write(warehouseMap[i][j]);
+            }
+
+            _console.WriteLine();
+        }
+        _console.WriteLine();
+    }
+
+    private static Point Move(Point position, Point directionChange) => new(position.Row + directionChange.Row, position.Col + directionChange.Col);
+
+    private (char[][] warehouseMap, List<Direction> directions) ReadWarehouseInformation(out Point robotPosition)
+    {
+        var lines = _reader.ReadLines();
+
+        List<char[]> warehouseMap = [];
+        var separatorLineIndex = -1;
+
+        robotPosition = new(-1, -1);
+        for (var i = 0; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            if (string.IsNullOrEmpty(line.Trim()))
+            {
+                separatorLineIndex = i;
+                break;
+            }
+
+            warehouseMap.Add(line.ToCharArray());
+
+            var robotIndex = line.IndexOf(Robot);
+            if (robotIndex >= 0)
+            {
+                robotPosition = new(i, robotIndex);
+            }
+        }
+
+        List<Direction> directions = [];
+        for (var i = separatorLineIndex + 1; i < lines.Length; i++)
+        {
+            directions.AddRange(lines[i].Select(c => (Direction)c));
+        }
+
+        return (warehouseMap.ToArray(), directions);
+    }
+
+    private enum Direction
+    {
+        Up = '^',
+        Right = '>',
+        Down = 'v',
+        Left = '<'
+    }
+
+    private record Point(int Row, int Col);
+}
