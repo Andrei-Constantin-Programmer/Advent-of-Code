d1f49a90048c06ed82ba33a44e036a30422cf3ff
Andrei Constantin <andrei.constantin@outlook.com>
2025-12-03 23:10:28 +0000

    Solved Y2025, D03

---
+++ Advent of Code.Tests/Challenge Solutions/Year 2025/ChallengeSolutionTests03.cs
@@ -0,0 +1,80 @@
+﻿using Advent_of_Code.Challenge_Solutions.Year_2025;
+using Advent_of_Code.Utilities;
+using NSubstitute;
+
+namespace Advent_of_Code.Tests.Challenge_Solutions.Year_2025;
+
+public class ChallengeSolutionTests03
+{
+    protected IConsole _consoleMock;
+    protected ISolutionReader<ChallengeSolution03> _readerMock;
+
+    private readonly ChallengeSolution03 _challengeSolution;
+
+    private readonly string[] _sampleInput =
+    [
+        "987654321111111",
+        "811111111111119",
+        "234234234234278",
+        "818181911112111"
+    ];
+
+    public ChallengeSolutionTests03()
+    {
+        _consoleMock = Substitute.For<IConsole>();
+        _readerMock = Substitute.For<ISolutionReader<ChallengeSolution03>>();
+
+        _readerMock.ReadLines()
+            .Returns(TestHelpers.GetInputFileContents(2025, 3));
+
+        _challengeSolution = new ChallengeSolution03(_consoleMock, _readerMock);
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(357)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(17155)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(3121910778619)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(169685670469164)));
+    }
+}
---
+++ Advent of Code/Challenge Solutions/Year 2025/ChallengeSolution03.cs
@@ -0,0 +1,71 @@
+﻿// Task: https://adventofcode.com/2025/day/3
+
+using Advent_of_Code.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2025;
+
+public class ChallengeSolution03(IConsole console, ISolutionReader<ChallengeSolution03> reader)
+    : ChallengeSolution<ChallengeSolution03>(console, reader)
+{
+    private const int BatteryJoltageDigitCount = 12;
+
+    public override void SolveFirstPart()
+    {
+        var batteries = _reader.ReadLines();
+        long outputJoltage = 0;
+
+        foreach (var battery in batteries)
+        {
+            var (firstDigit, firstDigitPosition) = battery[0..^1]
+                .Select((value, index) => (value, index))
+                .MaxBy(digit => digit.value);
+
+            var secondDigit = battery[(firstDigitPosition + 1)..].Max();
+
+            var batteryJoltage = long.Parse($"{firstDigit}{secondDigit}");
+
+            outputJoltage += batteryJoltage;
+        }
+
+        _console.WriteLine($"Output Joltage: {outputJoltage}");
+    }
+
+    public override void SolveSecondPart()
+    {
+        var batteries = _reader.ReadLines();
+        long outputJoltage = 0;
+
+        foreach (var battery in batteries)
+        {
+            outputJoltage += ComputeBatteryJoltage(battery, BatteryJoltageDigitCount);
+        }
+
+        _console.WriteLine($"Output Joltage: {outputJoltage}");
+    }
+
+    private static long ComputeBatteryJoltage(string battery, int digitCount)
+    {
+        Stack<char> joltageDigits = [];
+
+        for (var i = 0; i < battery.Length; i++)
+        {
+            var digit = battery[i];
+
+            while (CanPopDigitFromJoltage(joltageDigits, battery.Length, digitCount, i)
+                && joltageDigits.Peek() < digit)
+            {
+                _ = joltageDigits.Pop();
+            }
+
+            if (joltageDigits.Count < digitCount)
+            {
+                joltageDigits.Push(digit);
+            }
+        }
+
+        return long.Parse(string.Concat(joltageDigits.Reverse()));
+    }
+
+    private static bool CanPopDigitFromJoltage(Stack<char> stack, int batteryLength, int digitCount, int currentPosition)
+        => stack.Count > Math.Max(0, digitCount + currentPosition - batteryLength);
+}
