6f63dc130aeebc7e7e99c92f7fd08bfc821811a5
Andrei Constantin <andrei.constantin@outlook.com>
2025-12-04 19:37:18 +0000

    Solved Y2025, D04

---
+++ Advent of Code.Tests/Challenge Solutions/Year 2025/ChallengeSolutionTests04.cs
@@ -0,0 +1,86 @@
+﻿using Advent_of_Code.Challenge_Solutions.Year_2025;
+using Advent_of_Code.Utilities;
+using NSubstitute;
+
+namespace Advent_of_Code.Tests.Challenge_Solutions.Year_2025;
+
+public class ChallengeSolutionTests04
+{
+    protected IConsole _consoleMock;
+    protected ISolutionReader<ChallengeSolution04> _readerMock;
+
+    private readonly ChallengeSolution04 _challengeSolution;
+
+    private readonly string[] _sampleInput =
+    [
+        "..@@.@@@@.",
+        "@@@.@.@.@@",
+        "@@@@@.@.@@",
+        "@.@@@@..@.",
+        "@@.@@@@.@@",
+        ".@@@@@@@.@",
+        ".@.@.@.@@@",
+        "@.@@@.@@@@",
+        ".@@@@@@@@.",
+        "@.@.@@@.@."
+    ];
+
+    public ChallengeSolutionTests04()
+    {
+        _consoleMock = Substitute.For<IConsole>();
+        _readerMock = Substitute.For<ISolutionReader<ChallengeSolution04>>();
+
+        _readerMock.ReadLines()
+            .Returns(TestHelpers.GetInputFileContents(2025, 4));
+
+        _challengeSolution = new ChallengeSolution04(_consoleMock, _readerMock);
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(13)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(1480)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(43)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(8899)));
+    }
+}
---
+++ Advent of Code/Challenge Solutions/Year 2025/ChallengeSolution04.cs
@@ -0,0 +1,113 @@
+﻿// Task: https://adventofcode.com/2025/day/4
+
+using Advent_of_Code.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2025;
+
+public class ChallengeSolution04(IConsole console, ISolutionReader<ChallengeSolution04> reader)
+    : ChallengeSolution<ChallengeSolution04>(console, reader)
+{
+    private const char Empty = '.';
+    private const char PaperRoll = '@';
+    private const int MaxPaperRolls = 4;
+
+    public override void SolveFirstPart()
+    {
+        var grid = ReadGrid();
+
+        var reachablePaperRolls = 0;
+
+        for (var i = 0; i < grid.Length; i++)
+        {
+            for (var j = 0; j < grid.Length; j++)
+            {
+                if (grid[i][j] == Empty)
+                {
+                    continue;
+                }
+
+                var neighbours = GetNeighbours(grid, i, j).ToList();
+
+                if (neighbours.Count(neighbour => neighbour == PaperRoll) < MaxPaperRolls)
+                {
+                    reachablePaperRolls++;
+                }
+            }
+        }
+
+        _console.WriteLine($"Reachable paper rolls: {reachablePaperRolls}");
+    }
+
+    public override void SolveSecondPart()
+    {
+        var grid = ReadGrid();
+
+        var removedPaperRolls = 0;
+
+        var noMoreRollsAccessible = false;
+        while (!noMoreRollsAccessible)
+        {
+            noMoreRollsAccessible = true;
+            List<(int, int)> rollsToRemove = [];
+
+            for (var i = 0; i < grid.Length; i++)
+            {
+                for (var j = 0; j < grid.Length; j++)
+                {
+                    if (grid[i][j] == Empty)
+                    {
+                        continue;
+                    }
+
+                    var neighbours = GetNeighbours(grid, i, j).ToList();
+
+                    if (neighbours.Count(neighbour => neighbour == PaperRoll) < MaxPaperRolls)
+                    {
+                        noMoreRollsAccessible = false;
+                        rollsToRemove.Add((i, j));
+                    }
+                }
+            }
+
+            foreach (var (row, col) in rollsToRemove)
+            {
+                grid[row][col] = Empty;
+            }
+
+            removedPaperRolls += rollsToRemove.Count;
+        }
+
+        _console.WriteLine($"Removed paper rolls: {removedPaperRolls}");
+    }
+
+    private static IEnumerable<char> GetNeighbours(char[][] grid, int i, int j)
+    {
+        List<(int, int)> potentialNeighbouringPositions =
+            [
+                (i - 1, j),
+                (i + 1, j),
+                (i, j - 1),
+                (i, j + 1),
+                (i - 1, j - 1),
+                (i - 1, j + 1),
+                (i + 1, j - 1),
+                (i + 1, j + 1),
+            ];
+
+        foreach (var (row, col) in potentialNeighbouringPositions)
+        {
+            if (row < 0 || row >= grid.Length
+                || col < 0 || col >= grid[row].Length)
+            {
+                continue;
+            }
+
+            yield return grid[row][col];
+        }
+    }
+
+    private char[][] ReadGrid() => _reader
+        .ReadLines()
+        .Select(line => line.ToCharArray())
+        .ToArray();
+}
