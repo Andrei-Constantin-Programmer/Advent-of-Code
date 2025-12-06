42245f419281733a4f6c179ab67962b0236bdc85
Andrei Constantin <andrei.constantin@outlook.com>
2025-12-06 13:26:59 +0000

    Added solution for Y2025, D06

---
+++ Advent of Code.Tests/Challenge Solutions/Year 2025/ChallengeSolutionTests06.cs
@@ -0,0 +1,80 @@
+﻿using Advent_of_Code.Challenge_Solutions.Year_2025;
+using Advent_of_Code.Shared.Utilities;
+using NSubstitute;
+
+namespace Advent_of_Code.Tests.Challenge_Solutions.Year_2025;
+
+public class ChallengeSolutionTests06
+{
+    private readonly IConsole _consoleMock;
+    private readonly ISolutionReader<ChallengeSolution06> _readerMock;
+
+    private readonly ChallengeSolution06 _challengeSolution;
+
+    private readonly string[] _sampleInput =
+    [
+        "123 328  51 64 ",
+        " 45 64  387 23 ",
+        "  6 98  215 314",
+        "*   +   *   +  "
+    ];
+
+    public ChallengeSolutionTests06()
+    {
+        _consoleMock = Substitute.For<IConsole>();
+        _readerMock = Substitute.For<ISolutionReader<ChallengeSolution06>>();
+
+        _readerMock.ReadLines()
+            .Returns(TestHelpers.GetInputFileContents(2025, 6));
+
+        _challengeSolution = new ChallengeSolution06(_consoleMock, _readerMock);
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(4277556)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(5316572080628)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(3263827)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(11299263623062)));
+    }
+}
---
+++ Advent of Code/Challenge Solutions/Year 2025/ChallengeSolution06.cs
@@ -0,0 +1,158 @@
+﻿// Task: https://adventofcode.com/2025/day/6
+
+using Advent_of_Code.Shared;
+using Advent_of_Code.Shared.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2025;
+
+public class ChallengeSolution06(IConsole console, ISolutionReader<ChallengeSolution06> reader)
+    : ChallengeSolution<ChallengeSolution06>(console, reader)
+{
+    private const char Whitespace = ' ';
+    private const char Separator = '|';
+    
+    public override void SolveFirstPart()
+    {
+        var problems = ReadProblemsHuman();
+
+        var grandTotal = problems.Sum(p => p.Solve());
+        
+        Console.WriteLine($"Grand total: {grandTotal}");
+    }
+
+    public override void SolveSecondPart()
+    {
+        var problems = ReadProblemsCephalopod();
+
+        var grandTotal = problems.Sum(p => p.Solve());
+        
+        Console.WriteLine($"Grand total: {grandTotal}");
+    }
+
+    private List<Problem> ReadProblemsCephalopod()
+    {
+        List<Problem> problems = [];
+
+        var lines = ReadWorkSheetWithWhitespace();
+
+        for (var col = 0; col < lines[0].Length; col++)
+        {
+            var numbers = GetAllVerticalNumbers(lines, col);
+            var operation = GetOperationForColumn(lines, col);
+            
+            problems.Add(new(numbers, operation)); 
+        }
+            
+        return problems;
+    }
+
+    private static long[] GetAllVerticalNumbers(string[][] lines, int col)
+    {
+        List<long> numbers = [];
+
+        var numericRows = lines.SkipLast(1).Select(row => row[col]).ToArray();
+
+        for (var numericCol = numericRows[0].Length - 1; numericCol >= 0; numericCol--)
+        {
+            numbers.Add(ComputeNumberFromNumericRowsAndColumn(numericRows, numericCol));
+        }
+
+        return numbers.ToArray();
+    }
+
+    private static long ComputeNumberFromNumericRowsAndColumn(string[] numericRows, int numericCol)
+    {
+        List<char> numberAsDigits = [];
+
+        foreach (var row in numericRows)
+        {
+            var character = numericCol < row.Length ? row[numericCol] : Whitespace;
+            if (character is Whitespace)
+            {
+                continue;
+            }
+                    
+            numberAsDigits.Add(character);
+        }
+        
+        return long.Parse(new string(numberAsDigits.ToArray()));
+    }
+
+    private string[][] ReadWorkSheetWithWhitespace()
+    {
+        var lines = Reader
+            .ReadLines()
+            .Select(line => line.ToCharArray())
+            .ToArray();
+        
+        for (var charCol = 0; charCol < lines[0].Length; charCol++)
+        {
+            if (!lines.All(line => line[charCol] is Whitespace))
+            {
+                continue;
+            }
+
+            foreach (var line in lines)
+            {
+                line[charCol] = Separator;
+            }
+        }
+
+        var newLines = lines.Select(line => new string(line)).ToArray();
+        
+        return newLines
+            .Select(line => line.Split(Separator))
+            .ToArray();
+    }
+
+    private List<Problem> ReadProblemsHuman()
+    {
+        List<Problem> problems = [];
+
+        var lines = ReadWorksheetWithNoWhitespace();
+
+        for (var col = 0; col < lines[0].Length; col++)
+        {
+            var numbers = new long[lines.Length - 1];
+            for (var row = 0; row < lines.Length - 1; row++)
+            {
+                numbers[row] = long.Parse(lines[row][col]);
+            }
+
+            var operation = GetOperationForColumn(lines, col);
+            
+            problems.Add(new(numbers, operation));
+        }
+
+        return problems;
+    }
+
+    private static Operation GetOperationForColumn(string[][] lines, int col) => (Operation) lines[^1][col][0];
+
+    private string[][] ReadWorksheetWithNoWhitespace() =>
+        Reader.ReadLines()
+            .Select(line => line.Split(Whitespace, StringSplitOptions.RemoveEmptyEntries))
+            .ToArray();
+
+    private record Problem(long[] Numbers, Operation Operation)
+    {
+        public long Solve()
+        {
+            var x = Operation switch
+            {
+                Operation.Addition => Numbers.Sum(),
+                Operation.Multiplication => Numbers.Aggregate(1L, (acc, n) => acc * n),
+                
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            return x;
+        }
+    }
+
+    private enum Operation
+    {
+        Addition = '+',
+        Multiplication = '*'
+    }
+}
