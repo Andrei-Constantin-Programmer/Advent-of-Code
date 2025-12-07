9c9d7a155ff2e87f17ab454f9b02944b7cd8f5fd
Andrei Constantin <andrei.constantin@outlook.com>
2025-12-07 10:21:24 +0000

    Solved Y2025, D07

---
+++ Advent of Code.Tests/Challenge Solutions/Year 2025/ChallengeSolutionTests07.cs
@@ -0,0 +1,93 @@
+﻿using Advent_of_Code.Challenge_Solutions.Year_2025;
+using Advent_of_Code.Shared.Utilities;
+using Advent_of_Code.Tests.Shared;
+using NSubstitute;
+
+namespace Advent_of_Code.Tests.Challenge_Solutions.Year_2025;
+
+public class ChallengeSolutionTests07
+{
+    private readonly IConsole _consoleMock;
+    private readonly ISolutionReader<ChallengeSolution07> _readerMock;
+
+    private readonly ChallengeSolution07 _challengeSolution;
+
+    private readonly string[] _sampleInput =
+    [
+        ".......S.......",
+        "...............",
+        ".......^.......",
+        "...............",
+        "......^.^......",
+        "...............",
+        ".....^.^.^.....",
+        "...............",
+        "....^.^...^....",
+        "...............",
+        "...^.^...^.^...",
+        "...............",
+        "..^...^.....^..",
+        "...............",
+        ".^.^.^.^.^...^.",
+        "..............."
+    ];
+
+    public ChallengeSolutionTests07()
+    {
+        _consoleMock = Substitute.For<IConsole>();
+        _readerMock = Substitute.For<ISolutionReader<ChallengeSolution07>>();
+
+        _readerMock.ReadLines()
+            .Returns(TestHelpers.GetInputFileContents(2025, 7));
+
+        _challengeSolution = new ChallengeSolution07(_consoleMock, _readerMock);
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(21)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(1638)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(40)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(7759107121385)));
+    }
+}
---
+++ Advent of Code/Challenge Solutions/Year 2025/ChallengeSolution07.cs
@@ -0,0 +1,121 @@
+﻿// Task: https://adventofcode.com/2025/day/7
+
+using Advent_of_Code.Shared;
+using Advent_of_Code.Shared.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2025;
+
+public class ChallengeSolution07(IConsole console, ISolutionReader<ChallengeSolution07> reader)
+    : ChallengeSolution<ChallengeSolution07>(console, reader)
+{
+    private const char BeamStart = 'S';
+    private const char Splitter = '^';
+    
+    public override void SolveFirstPart()
+    {
+        var lines = Reader.ReadLines();
+        var splittersByRow = ReadTachyonManifold(lines, out var beamStart);
+
+        HashSet<int> beamColumns = [beamStart.Col];
+        var beamSplitCount = 0;
+
+        for (var row = 1; row < lines.Length; row++)
+        {
+            HashSet<int> newBeamColumns = [];
+
+            foreach (var beamColumn in beamColumns)
+            {
+                if (beamColumn < 0 || beamColumn >= lines[row].Length)
+                {
+                    continue;
+                }
+
+                if (!splittersByRow[row].Contains(beamColumn))
+                {
+                    newBeamColumns.Add(beamColumn);
+                    continue;
+                }
+
+                beamSplitCount++;
+                newBeamColumns.Add(beamColumn - 1);
+                newBeamColumns.Add(beamColumn + 1);
+            }
+            
+            beamColumns = newBeamColumns;
+        }
+        
+        Console.WriteLine($"Beam split: {beamSplitCount} times");
+    }
+
+    public override void SolveSecondPart()
+    {
+        var lines = Reader.ReadLines();
+        var splittersByRow = ReadTachyonManifold(lines, out var beamStart);
+
+        var timelines = ComputeTimelines(lines, splittersByRow, [], beamStart.Row + 1, beamStart.Col);
+        
+        Console.WriteLine($"Active timelines: {timelines}");
+    }
+    
+    private static long ComputeTimelines(
+        string[] lines, 
+        Dictionary<int, List<int>> splittersByRow,
+        Dictionary<(int, int), long> timelinesFromRowCol,
+        int currentRow,
+        int tachyonColumn)
+    {
+        if (tachyonColumn < 0 || tachyonColumn > lines.Length)
+        {
+            return 0;
+        }
+        
+        if (currentRow >= splittersByRow.Count)
+        {
+            return 1;
+        }
+
+        if (!splittersByRow[currentRow].Contains(tachyonColumn))
+        {
+            if (timelinesFromRowCol.TryGetValue((currentRow, tachyonColumn), out var timelines))
+            {
+                return timelines;
+            }
+            
+            timelines = ComputeTimelines(lines, splittersByRow, timelinesFromRowCol, currentRow + 1, tachyonColumn); 
+            timelinesFromRowCol.Add((currentRow, tachyonColumn), timelines);
+            return timelines;
+        }
+
+        var timelinesLeft = ComputeTimelines(lines, splittersByRow, timelinesFromRowCol, currentRow + 1, tachyonColumn - 1);
+        var timelinesRight = ComputeTimelines(lines, splittersByRow, timelinesFromRowCol, currentRow + 1, tachyonColumn + 1);
+
+        return timelinesLeft + timelinesRight;
+    }
+
+    private static Dictionary<int, List<int>> ReadTachyonManifold(string[] lines, out Location beamStart)
+    {
+        Dictionary<int, List<int>> splittersByRow = [];
+        beamStart = new(0, 0);
+
+        for (var row = 0; row < lines.Length; row++)
+        {
+            splittersByRow.Add(row, []);
+            for (var col = 0; col < lines[row].Length; col++)
+            {
+                switch (lines[row][col])
+                {
+                    case BeamStart:
+                        beamStart = new Location(row, col);
+                        continue;
+                    case Splitter:
+                        splittersByRow[row].Add(col);
+                        break;
+                }
+            }
+        }
+
+        return splittersByRow;
+    }
+
+    private record struct Location(int Row, int Col);
+}
