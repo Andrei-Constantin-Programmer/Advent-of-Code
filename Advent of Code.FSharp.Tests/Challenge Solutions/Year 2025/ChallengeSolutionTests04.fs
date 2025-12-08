6d4be42741ebb3f434950880ce8ad8d0f954692e
Andrei Constantin <andrei.constantin@outlook.com>
2025-12-08 21:21:10 +0000

    Solved Y2025, D04 in F#.

---
+++ Advent of Code.FSharp.Tests/Challenge Solutions/Year 2025/ChallengeSolutionTests04.fs
@@ -0,0 +1,77 @@
+﻿namespace Advent_of_Code.FSharp.Challenge_Solutions.Year_2025.Tests
+
+open Advent_of_Code.Tests.Shared
+open Xunit
+open Advent_of_Code.FSharp.Challenge_Solutions.Year_2025.ChallengeSolution04
+open Advent_of_Code.FSharp.Tests.TestUtilities
+open TestExtensions
+
+module ChallengeSolutionTests04 =
+
+    let getSampleInput() : string list =
+        [
+            "..@@.@@@@.";
+            "@@@.@.@.@@";
+            "@@@@@.@.@@";
+            "@.@@@@..@.";
+            "@@.@@@@.@@";
+            ".@@@@@@@.@";
+            ".@.@.@.@@@";
+            "@.@@@.@@@@";
+            ".@@@@@@@@.";
+            "@.@.@@@.@."
+        ]
+        
+    let getRealInput() : string list = TestHelpers.GetInputFileContents(2025, 4) |> List.ofArray
+
+    [<Fact>]
+    let ``SolveFirstPart with sample input`` () =
+        // Arrange
+        let writeLineFake = WriteLineFake()
+
+        let challengeSolution = Solution(getSampleInput, writeLineFake.WriteLine)
+
+        // Act
+        challengeSolution.SolveFirstPart()
+
+        // Assert
+        writeLineFake.VerifyCalledContainingInt 13 1
+        
+    [<Fact>]
+    let ``SolveFirstPart with real input``() =
+        // Arrange
+        let writeLineFake = WriteLineFake()
+        
+        let challengeSolution = Solution(getRealInput, writeLineFake.WriteLine)
+    
+        // Act
+        challengeSolution.SolveFirstPart()
+
+        // Assert
+        writeLineFake.VerifyCalledContainingInt 1480 1        
+
+    [<Fact>]
+    let ``SolveSecondPart with sample input`` () =
+        // Arrange
+        let writeLineFake = WriteLineFake()
+
+        let challengeSolution = Solution(getSampleInput, writeLineFake.WriteLine)
+
+        // Act
+        challengeSolution.SolveSecondPart()
+
+        // Assert
+        writeLineFake.VerifyCalledContainingInt 43 1
+
+    [<Fact>]
+    let ``SolveSecondPart with real input``() =
+        // Arrange
+        let writeLineFake = WriteLineFake()
+        
+        let challengeSolution = Solution(getRealInput, writeLineFake.WriteLine)
+    
+        // Act
+        challengeSolution.SolveSecondPart()
+
+        // Assert
+        writeLineFake.VerifyCalledContainingInt 8899 1       
---
+++ Advent of Code.FSharp/Challenge Solutions/Year 2025/ChallengeSolution04.fs
@@ -0,0 +1,99 @@
+﻿namespace Advent_of_Code.FSharp.Challenge_Solutions.Year_2025
+
+open Advent_of_Code.Shared
+open Advent_of_Code.Shared.Utilities
+open Advent_of_Code.FSharp.Utilities
+
+module ChallengeSolution04 =
+
+    [<Literal>]
+    let maxAdjacentPaperRolls = 4
+    
+    type Floor =
+        | PaperRoll
+        | Empty
+        
+    type Grid = Floor[,]
+    
+    type PaperRollLocation = int * int
+    
+    type Solution(getInput: unit -> string list, writeLine: string -> unit) =
+        
+        let readFloorGrid() : Grid =
+            getInput()
+            |> Seq.map
+                ^<| Seq.map (function
+                        | '@' -> PaperRoll
+                        | _   -> Empty
+                    )
+            |> array2D
+            
+        let getAllPaperRolls (grid: Grid) : PaperRollLocation list =
+            grid
+            |> Array2D.mapi (fun row col value ->
+                             match value with
+                             | PaperRoll -> Some (row, col)
+                             | _ -> None)
+            |> Seq.cast<PaperRollLocation option>
+            |> Seq.choose id
+            |> List.ofSeq
+            
+        let getNeighbouringPaperRolls (grid: Grid) (row: int, col: int) =
+            [
+                (row - 1, col)
+                (row + 1, col)
+                (row, col - 1)
+                (row, col + 1)
+                (row - 1, col - 1)
+                (row - 1, col + 1)
+                (row + 1, col - 1)
+                (row + 1, col + 1)
+            ]
+            |> List.filter (fun (r, c) -> r >= 0 && r < (grid |> Array2D.length1)
+                                           && c >= 0 && c < (grid |> Array2D.length2)
+                                           && grid[r, c] = PaperRoll)
+            
+        let isAccessible (grid: Grid) (roll: PaperRollLocation) =
+            getNeighbouringPaperRolls grid roll
+                |> List.length < maxAdjacentPaperRolls 
+            
+        let removeAllAccessibleRolls (grid: Grid) =
+            let rec removeAllAccessibleInner removedSoFar =
+                let rollsToRemove =
+                    grid
+                    |> getAllPaperRolls
+                    |> List.filter (isAccessible grid)
+                
+                match rollsToRemove with
+                | [] -> removedSoFar
+                | _ -> rollsToRemove
+                       |> List.iter (fun (row, col) -> grid[row, col] <- Empty)
+                       
+                       removeAllAccessibleInner (removedSoFar + rollsToRemove.Length)
+            
+            removeAllAccessibleInner 0
+                        
+        member _.SolveFirstPart() =
+            let grid = readFloorGrid()
+
+            let reachableRolls =
+                grid
+                |> getAllPaperRolls
+                |> List.filter (isAccessible grid)
+                |> List.length
+                
+            writeLine $"Reachable paper rolls: {reachableRolls}"
+        
+        member _.SolveSecondPart() : unit =
+            let grid = readFloorGrid()
+            let removed = removeAllAccessibleRolls grid
+            
+            writeLine $"Removed paper rolls: {removed}"
+
+    type ChallengeSolution04(console: IConsole, reader: ISolutionReader<ChallengeSolution04>) =
+        inherit ChallengeSolution<ChallengeSolution04>(console, reader)
+
+        let solution = Solution(reader.ReadLines >> List.ofArray, console.WriteLine)
+
+        override this.SolveFirstPart() = solution.SolveFirstPart()
+        override this.SolveSecondPart() = solution.SolveSecondPart()
