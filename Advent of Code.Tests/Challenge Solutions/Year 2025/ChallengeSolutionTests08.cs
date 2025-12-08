d312e58f44d85a0abea352572fb884eeb120eccc
Andrei Constantin <andrei.constantin@outlook.com>
2025-12-08 18:11:53 +0000

    Attempt 1 for Y2025, D08.

---
+++ Advent of Code.Tests/Challenge Solutions/Year 2025/ChallengeSolutionTests08.cs
@@ -0,0 +1,98 @@
+﻿using Advent_of_Code.Challenge_Solutions.Year_2025;
+using Advent_of_Code.Shared.Utilities;
+using Advent_of_Code.Tests.Shared;
+using NSubstitute;
+
+namespace Advent_of_Code.Tests.Challenge_Solutions.Year_2025;
+
+public class ChallengeSolutionTests08
+{
+    private readonly IConsole _consoleMock;
+    private readonly ISolutionReader<ChallengeSolution08> _readerMock;
+
+    private readonly ChallengeSolution08 _challengeSolution;
+
+    private readonly string[] _sampleInput =
+    [
+        "10",
+        "162,817,812",
+        "57,618,57",
+        "906,360,560",
+        "592,479,940",
+        "352,342,300",
+        "466,668,158",
+        "542,29,236",
+        "431,825,988",
+        "739,650,466",
+        "52,470,668",
+        "216,146,977",
+        "819,987,18",
+        "117,168,530",
+        "805,96,715",
+        "346,949,466",
+        "970,615,88",
+        "941,993,340",
+        "862,61,35",
+        "984,92,344",
+        "425,690,689"
+    ];
+
+    public ChallengeSolutionTests08()
+    {
+        _consoleMock = Substitute.For<IConsole>();
+        _readerMock = Substitute.For<ISolutionReader<ChallengeSolution08>>();
+
+        _readerMock.ReadLines()
+            .Returns(TestHelpers.GetInputFileContents(2025, 8));
+
+        _challengeSolution = new ChallengeSolution08(_consoleMock, _readerMock);
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(40)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsInt(1638)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInput()
+    {
+        // Arrange
+        _readerMock.ReadLines()
+            .Returns(_sampleInput);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(40)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(7759107121385)));
+    }
+}
---
+++ Advent of Code/Challenge Solutions/Year 2025/ChallengeSolution08.cs
@@ -0,0 +1,139 @@
+﻿// Task: https://adventofcode.com/2025/day/8
+
+
+using Advent_of_Code.Shared;
+using Advent_of_Code.Shared.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2025;
+
+public class ChallengeSolution08(IConsole console, ISolutionReader<ChallengeSolution08> reader)
+    : ChallengeSolution<ChallengeSolution08>(console, reader)
+{
+    public override void SolveFirstPart()
+    {
+        var junctionBoxes = ReadJunctionBoxLocations(out var pairsToConnect);
+        HydrateJunctionBoxesWithDistanceToOthers(junctionBoxes);
+
+        HydrateJunctionBoxesWithTheTopConnectedPairs(pairsToConnect, junctionBoxes);
+
+        var circuitLengths = ComputeCircuitLengths(junctionBoxes);
+
+        var topThreeCircuits = circuitLengths
+            .OrderDescending()
+            .Take(3)
+            .Aggregate(1, (acc, x) => acc * x);
+        
+        Console.WriteLine($"Top 3 circuit sizes: {topThreeCircuits}");
+    }
+
+    public override void SolveSecondPart()
+    {
+
+    }
+    
+    private static List<int> ComputeCircuitLengths(List<JunctionBox> junctionBoxes)
+    {
+        List<int> circuitLengths = [];
+        HashSet<JunctionBox> alreadyChecked = [];
+        foreach (var junctionBox in junctionBoxes)
+        {
+            if (alreadyChecked.Contains(junctionBox))
+            {
+                continue;
+            }
+
+            circuitLengths.Add(1 + junctionBox.Neighbours.Count);
+            alreadyChecked.Add(junctionBox);
+            foreach (var jb in junctionBox.Neighbours)
+            {
+                alreadyChecked.Add(jb);
+            }
+        }
+
+        return circuitLengths;
+    }
+
+    private static void HydrateJunctionBoxesWithTheTopConnectedPairs(int pairsToConnect, List<JunctionBox> junctionBoxes)
+    {
+        HashSet<(JunctionBox, JunctionBox)> alreadyConnected = [];
+        for (var pairsConnected = 0; pairsConnected < pairsToConnect; pairsConnected++)
+        {
+            var newConnection = junctionBoxes
+                .SelectMany(
+                    jb => jb.DistanceTo,
+                    (jb, kvp) => new
+                    {
+                        JunctionBox = jb,
+                        Neighbour = kvp.Key,
+                        Distance = kvp.Value
+                    })
+                .Where(x => !alreadyConnected.Contains((x.JunctionBox, x.Neighbour)))
+                .MinBy(x => x.Distance)!;
+
+            var jb1 = newConnection.JunctionBox;
+            var jb2 = newConnection.Neighbour;
+            
+            foreach (var neighbour in jb2.Neighbours)
+            {
+                jb1.Neighbours.Add(neighbour);
+                alreadyConnected.Add((jb1, neighbour));
+                alreadyConnected.Add((neighbour, jb1));
+            }
+            
+            foreach (var neighbour in jb1.Neighbours)
+            {
+                jb2.Neighbours.Add(neighbour);
+                alreadyConnected.Add((jb2, neighbour));
+                alreadyConnected.Add((neighbour, jb2));
+            }
+            
+            jb1.Neighbours.Add(jb2);
+            jb2.Neighbours.Add(jb1);
+            alreadyConnected.Add((jb1, jb2));
+            alreadyConnected.Add((jb2, jb1));
+        }
+    }
+    
+    private static void HydrateJunctionBoxesWithDistanceToOthers(List<JunctionBox> junctionBoxes)
+    {
+        for (var i = 0; i < junctionBoxes.Count - 1; i++)
+        {
+            for (var j = i + 1; j < junctionBoxes.Count; j++)
+            {
+                var distance = EuclideanDistance(junctionBoxes[i], junctionBoxes[j]);
+                junctionBoxes[i].DistanceTo.Add(junctionBoxes[j], distance);
+                junctionBoxes[j].DistanceTo.Add(junctionBoxes[i], distance);
+            }
+        }
+    }
+    
+    private static double EuclideanDistance(JunctionBox jb1, JunctionBox jb2)
+    {
+        var x = jb1.X - jb2.X;
+        var y = jb1.Y - jb2.Y;
+        var z = jb1.Z - jb2.Z;
+        
+        return Math.Sqrt(x * x + y * y + z * z);
+    }
+
+    private List<JunctionBox> ReadJunctionBoxLocations(out int pairsToConnect)
+    {
+        var lines = Reader.ReadLines();
+        pairsToConnect = int.Parse(lines[0]);
+        List<JunctionBox> junctionBoxes = [];
+
+        foreach (var line in lines[1..])
+        {
+            var coords = line
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+            
+            junctionBoxes.Add(new JunctionBox(coords[0], coords[1], coords[2], [], []));
+        }
+
+        return junctionBoxes;
+    }
+    
+    private record JunctionBox(int X, int Y, int Z, Dictionary<JunctionBox, double> DistanceTo, HashSet<JunctionBox> Neighbours);
+}
