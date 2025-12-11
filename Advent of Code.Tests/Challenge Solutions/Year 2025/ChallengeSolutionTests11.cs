593a6cdaa909c864a100349a347e188fae0294a3
Andrei Constantin <andrei.constantin@outlook.com>
2025-12-11 21:22:58 +0000

    Solved Y2025, D11 (particularly happy about my solution :D)

---
+++ Advent of Code.Tests/Challenge Solutions/Year 2025/ChallengeSolutionTests11.cs
@@ -0,0 +1,104 @@
+﻿using Advent_of_Code.Challenge_Solutions.Year_2025;
+using Advent_of_Code.Shared.Utilities;
+using Advent_of_Code.Tests.Shared;
+using NSubstitute;
+
+namespace Advent_of_Code.Tests.Challenge_Solutions.Year_2025;
+
+public class ChallengeSolutionTests11
+{
+    private readonly IConsole _consoleMock;
+    private readonly ISolutionReader<ChallengeSolution11> _readerMock;
+
+    private readonly ChallengeSolution11 _challengeSolution;
+
+    public ChallengeSolutionTests11()
+    {
+        _consoleMock = Substitute.For<IConsole>();
+        _readerMock = Substitute.For<ISolutionReader<ChallengeSolution11>>();
+
+        _readerMock.ReadLines()
+            .Returns(TestHelpers.GetInputFileContents(2025, 11));
+
+        _challengeSolution = new ChallengeSolution11(_consoleMock, _readerMock);
+    }
+
+    [Fact]
+    public void SolveFirstPart_SampleInput()
+    {
+        // Arrange
+        string[] sampleInput =
+        [
+            "aaa: you hhh",
+            "you: bbb ccc",
+            "bbb: ddd eee",
+            "ccc: ddd eee fff",
+            "ddd: ggg",
+            "eee: out",
+            "fff: out",
+            "ggg: out",
+            "hhh: ccc fff iii",
+            "iii: out"
+        ];
+        
+        _readerMock.ReadLines()
+            .Returns(sampleInput);
+
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(5)));
+    }
+
+    [Fact]
+    public void SolveFirstPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveFirstPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(674)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_SampleInput()
+    {
+        // Arrange
+        string[] sampleInput =
+        [
+            "svr: aaa bbb",
+            "aaa: fft",
+            "fft: ccc",
+            "bbb: tty",
+            "tty: ccc",
+            "ccc: ddd eee",
+            "ddd: hub",
+            "hub: fff",
+            "eee: dac",
+            "dac: fff",
+            "fff: ggg hhh",
+            "ggg: out",
+            "hhh: out"
+        ];
+        
+        _readerMock.ReadLines()
+            .Returns(sampleInput);
+
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(2)));
+    }
+
+    [Fact]
+    public void SolveSecondPart_RealInput()
+    {
+        // Act
+        _challengeSolution.SolveSecondPart();
+
+        // Assert
+        _consoleMock.Received().WriteLine(Arg.Is<string>(s => s.ContainsLong(438314708837664L)));
+    }
+}
---
+++ Advent of Code/Challenge Solutions/Year 2025/ChallengeSolution11.cs
@@ -0,0 +1,126 @@
+﻿// Task: https://adventofcode.com/2025/day/11
+
+using Advent_of_Code.Shared;
+using Advent_of_Code.Shared.Utilities;
+
+namespace Advent_of_Code.Challenge_Solutions.Year_2025;
+
+public class ChallengeSolution11(IConsole console, ISolutionReader<ChallengeSolution11> reader)
+    : ChallengeSolution<ChallengeSolution11>(console, reader)
+{
+    private const string YourDeviceName = "you";
+    private const string OutputDeviceName = "out";
+
+    private const string ServerRackName = "svr";
+    private const string DigitalToAnalogConverterName = "dac";
+    private const string FastFourierTransformName = "fft";
+    
+    public override void SolveFirstPart()
+    {
+        _ = ReadDevices(YourDeviceName, out var startDevice);
+
+        var pathCount = ComputePathCountToOutputDevice(startDevice, OutputDeviceName);
+
+        Console.WriteLine($"Paths to {OutputDeviceName}: {pathCount}");
+    }
+
+    public override void SolveSecondPart()
+    {
+        var devicesByName = ReadDevices(ServerRackName, out var startDevice);
+
+        long pathCount = 0;
+
+        var dacDevice = devicesByName[DigitalToAnalogConverterName];
+        var fftDevice = devicesByName[FastFourierTransformName];
+
+        var pathsFromDacToFft = ComputePathCountToOutputDevice(dacDevice, FastFourierTransformName);
+        
+        if (pathsFromDacToFft > 0)
+        {
+            var pathsToDac = ComputePathCountToOutputDevice(startDevice, DigitalToAnalogConverterName);
+            var pathsToOut = ComputePathCountToOutputDevice(fftDevice, OutputDeviceName);
+
+            pathCount = pathsToDac * pathsFromDacToFft * pathsToOut;
+        }
+        else
+        {
+            var pathsFromFftToDac = ComputePathCountToOutputDevice(fftDevice, DigitalToAnalogConverterName);
+            if (pathsFromFftToDac > 0)
+            {
+                var pathsToFft = ComputePathCountToOutputDevice(startDevice, FastFourierTransformName);
+                var pathsToOut = ComputePathCountToOutputDevice(dacDevice, OutputDeviceName);
+
+                pathCount = pathsToFft * pathsFromFftToDac * pathsToOut;
+            }
+        }
+        
+        Console.WriteLine($"Paths to {OutputDeviceName} through {DigitalToAnalogConverterName} and {FastFourierTransformName}: {pathCount}");
+    }
+    
+    private static long ComputePathCountToOutputDevice(Device startDevice, string endDeviceName)
+    {
+        Dictionary<Device, long> pathsToOutFromDevice = [];
+        
+        return ComputePathsFromDevice(startDevice);
+
+        long ComputePathsFromDevice(Device device)
+        {
+            if (pathsToOutFromDevice.TryGetValue(device, out var paths))
+            {
+                return paths;
+            }
+
+            if (device.Name == endDeviceName)
+            {
+                return 1;
+            }
+
+            paths = device.Outputs.Sum(ComputePathsFromDevice);
+            pathsToOutFromDevice.Add(device, paths);
+            
+            return paths;
+        }
+    }
+
+    private Dictionary<string, Device> ReadDevices(string startDeviceName, out Device startDevice)
+    {
+        var lines = Reader.ReadLines();
+
+        Dictionary<string, Device> devicesByName = [];
+        Dictionary<string, string[]> deviceNameToOutputs = [];
+
+        foreach (var line in lines)
+        {
+            var elements = line
+                .Split(':', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            
+            var deviceName = elements[0];
+            var outputs = elements[1]
+                .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            
+            devicesByName.Add(deviceName, new Device(deviceName));
+            deviceNameToOutputs.Add(deviceName, outputs);
+        }
+        devicesByName.Add(OutputDeviceName, new Device(OutputDeviceName));
+        deviceNameToOutputs.Add(OutputDeviceName, []);
+
+        startDevice = devicesByName[startDeviceName];
+
+        foreach (var deviceName in devicesByName.Keys)
+        {
+            devicesByName[deviceName].Outputs = 
+                deviceNameToOutputs[deviceName]
+                .Select(d => devicesByName[d])
+                .ToHashSet();
+        }
+
+        return devicesByName;
+    }
+
+    private class Device(string name)
+    {
+        public string Name { get; } = name;
+
+        public HashSet<Device> Outputs { get; set; } = [];
+    }
+}
